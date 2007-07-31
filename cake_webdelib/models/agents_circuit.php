075bf2f24d63b263927793c3e6f1d6fafeb91373
laurie <laurie@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-07-31 12:09:30 +0000

    Ajout du model agents_circuit

---
+++ cake_webdelib/models/agents_circuit.php
@@ -0,0 +1,34 @@
+<?php
+class AgentsCircuit extends AppModel {
+
+	var $name = 'AgentsCircuit';
+	//var $primaryKey = 'agent_id';
+	var $validate = array(
+		'circuit_id' => VALID_NOT_EMPTY,
+		'position' => VALID_NOT_EMPTY,
+	);
+
+	var $useTable="agents_circuits";
+	
+	var $belongsTo = array(
+			'Circuit' =>
+				array('className' => 'Circuit',
+						'foreignKey' => 'circuit_id',
+						'conditions' => '',
+						'fields' => '',
+						'order' => '',
+						'counterCache' => ''
+				),
+
+			'Agent' =>
+				array('className' => 'Agent',
+						'foreignKey' => 'agent_id',
+						'conditions' => '',
+						'fields' => '',
+						'order' => '',
+						'counterCache' => ''
+				),
+
+	);
+}
+?>
