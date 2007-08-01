cdfec201c57ca88f96637955ada71163bb20c4cf
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-08-01 08:58:02 +0000

    Ajout de l'helper Fck

---
+++ cake_webdelib/views/helpers/fck.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Created on 31 juil. 07
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+class FckHelper extends Helper
+{
+    function load($id, $toolbar = 'Default') {
+        foreach (explode('[', str_replace(']','', $id)) as $v) {
+			$did .= ucfirst($v);
+		}
+
+        return <<<FCK_CODE
+<script type="text/javascript">
+fckLoader_$did = function () {
+    var bFCKeditor_$did = new FCKeditor('$id');
+    bFCKeditor_$did.BasePath = 'http://francois/cake/js/';
+    bFCKeditor_$did.ToolbarSet = '$toolbar';
+    bFCKeditor_$did.ReplaceTextarea();
+}
+fckLoader_$did();
+</script>
+FCK_CODE;
+    }
+} 
+?>
