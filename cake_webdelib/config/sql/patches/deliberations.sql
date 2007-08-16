733ed686fc6c447c209fd81e00d213c5dd3d34ed
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-08-16 08:01:57 +0000

    Modification des champs text_projet/texte_synthese en LONGBLOB et du champs date_session en seance_id

--- cake_webdelib/config/sql/patches/deliberations.sql
+++ cake_webdelib/config/sql/patches/deliberations.sql
@@ -1,3 +1,6 @@
 -- 14/08/2007 : Changement du format des champs de la BDD dans la table délibérations texte_projet et texte_synthèse afin de pouvoir accueillir de grands textes
 ALTER TABLE `deliberations` CHANGE `texte_projet` `texte_projet` LONGBLOB;
-ALTER TABLE `deliberations` CHANGE `texte_synthese` `texte_projet` LONGBLOB;
+ALTER TABLE `deliberations` CHANGE `texte_synthese` `texte_synthese` LONGBLOB;
+
+--14/08/2007 : changement du champs date_sesion en seance_id pour devenir une clé etrangere de la table seance
+ALTER TABLE `deliberations` CHANGE `date_session` `seance_id` INT NULL  
