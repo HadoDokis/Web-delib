a7181318288a3a470ae43d30c017c0cddf384705
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-08-21 09:52:00 +0000

    Remplacement des champs "agent" par "user"

--- trunk/cake_webdelib/models/commentaire.php
+++ trunk/cake_webdelib/models/commentaire.php
@@ -4,7 +4,7 @@ class Commentaire extends AppModel {
 	var $name = 'Commentaire';
 	var $validate = array(
 		'delib_id' => VALID_NOT_EMPTY,
-		'agent_id' => VALID_NOT_EMPTY,
+		'user_id' => VALID_NOT_EMPTY,
 		'texte' => VALID_NOT_EMPTY,
 	);
 
