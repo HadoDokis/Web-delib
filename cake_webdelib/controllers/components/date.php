f23be636622855e142f781a18d496e455d990044
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-08-22 09:24:36 +0000

    Pour la traduction des dates.

---
+++ cake_webdelib/controllers/components/date.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * CrÃ©ation : 20 janv. 2006
+ * Christophe Espiau
+ * 
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+class DateComponent extends Object {
+	
+	var $days = array ('Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi');
+	
+	var $months = array ('','Janvier','Février','Mars','Avril','Mai','Juin',
+						'Juillet','Août','Septembre','Octobre','Novembre','Décembre');
+
+		
+}
+
+?>
