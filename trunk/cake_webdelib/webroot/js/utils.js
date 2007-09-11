dc808956020f5ada2b822b1c12f57582eb3235fe
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-11 15:08:46 +0000

    Ajout de la fonction add_field

--- trunk/cake_webdelib/webroot/js/utils.js
+++ trunk/cake_webdelib/webroot/js/utils.js
@@ -9,8 +9,37 @@ function montre(id) {
 
 
 
-function changeService(params)
-{
+	function changeService(params)
+	{
   	var url = params.id+"services/changeService/"+params.value;
 	document.location=url;
-}
+	}
+
+
+	function add_field() {
+
+	var a = document.getElementById('lien_annexe');
+	a.firstChild.nodeValue = 'Joindre une autre annexe';
+
+  var d = document.getElementById('cible');
+  var n = d.childNodes.length;
+  var p = document.createElement("p");
+  d.appendChild(p);
+
+  var input1 = document.createElement('input');
+  input1.id = 'AnnexeTitre_'+n;
+  input1.type = 'text';
+  input1.size = '30';
+  input1.name = 'titre_'+n;
+  p.appendChild(input1);
+
+  var input2 = document.createElement('input');
+  input2.id = 'AnnexeFile_'+n;
+  input2.type = 'file';
+  input2.size = '40';
+  input2.name = 'file_'+n;
+  p.appendChild(input2);
+
+  document.getElementById('cible').style.visibility = 'visible';
+
+	}
