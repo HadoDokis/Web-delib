d99ed1c98202630165d5be417852eba2e74351da
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-14 13:28:11 +0000

    Ajout de la fonction del_field

--- trunk/cake_webdelib/webroot/js/utils.js
+++ trunk/cake_webdelib/webroot/js/utils.js
@@ -9,37 +9,56 @@ function montre(id) {
 
 
 
-	function changeService(params)
-	{
-  	var url = params.id+"services/changeService/"+params.value;
+function changeService(params)
+{
+	var url = params.id+"services/changeService/"+params.value;
 	document.location=url;
-	}
+}
 
 
-	function add_field() {
+function add_field() {
 
 	var a = document.getElementById('lien_annexe');
 	a.firstChild.nodeValue = 'Joindre une autre annexe';
 
-  var d = document.getElementById('cible');
-  var n = d.childNodes.length;
-  var p = document.createElement("p");
-  d.appendChild(p);
+ 	var d = document.getElementById('cible');
+	var n = d.childNodes.length;
+	var p = document.createElement("p");
+	p.id = n;
+ 	d.appendChild(p);
 
-  var input1 = document.createElement('input');
-  input1.id = 'AnnexeTitre_'+n;
-  input1.type = 'text';
-  input1.size = '30';
-  input1.name = 'titre_'+n;
-  p.appendChild(input1);
+	
+	var input1 = document.createElement('input');
+	input1.id = 'AnnexeTitre_'+n;
+ 	input1.type = 'text';
+	input1.size = '30';
+	input1.name = 'titre_'+n;
+	p.appendChild(input1);
 
-  var input2 = document.createElement('input');
-  input2.id = 'AnnexeFile_'+n;
-  input2.type = 'file';
-  input2.size = '40';
-  input2.name = 'file_'+n;
-  p.appendChild(input2);
+	var input2 = document.createElement('input');
+	input2.id = 'AnnexeFile_'+n;
+	input2.type = 'file';
+	input2.size = '40';
+	input2.name = 'file_'+n;
+	p.appendChild(input2);
+	var br = document.createElement('br');
+	p.appendChild(br);
+  
+	var link = document.createElement('a');
+	link.id = 'Lien_'+n;
+	link.href = 'javascript:del_field('+n+')';
+	text = document.createTextNode('Supprimer');	
+	link.appendChild(text);
+	p.appendChild(link);
 
-  document.getElementById('cible').style.visibility = 'visible';
+	document.getElementById('cible').style.visibility = 'visible';
 
-	}
+}
+	
+function del_field(node)
+{
+	var node = document.getElementById(node);
+	var parent = node.parentNode;
+	parent.removeChild(node);
+}
+	
