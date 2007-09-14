ee0cc84a972da8c49f874abe8f8e6d1d9b0afc71
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-14 07:34:52 +0000

    Ajout du controller Commentaires

---
+++ cake_webdelib/controllers/commentaires_controller.php
@@ -0,0 +1,57 @@
+<?php
+class CommentairesController extends AppController {
+
+	function add($delib_id=null) {
+		if (empty($this->data)) {
+			$this->set('delib_id',$delib_id);
+		} else {
+				$this->data['Commentaire']['agent_id'] = $this->Session->read('user.User.id');
+			if ($this->Commentaire->save($this->data)) {
+				$this->redirect('/deliberations/traiter/'.$this->data['Commentaire']['delib_id']);
+			} else {
+				$this->Session->setFlash('Please correct errors below.');
+			}
+		}
+	}
+	
+	function edit($id = null,$delib_id=null) {
+		if (empty($this->data)) {
+			if (!$id) {
+				$this->Session->setFlash('Invalid id for Commentaire');
+				$this->redirect('/deliberations/traiter/'.$delib_id);
+			}
+			$this->set('delib_id',$delib_id);
+			$this->data = $this->Commentaire->read(null, $id);
+			} else {
+				$this->data['Commentaire']['agent_id'] = $this->Session->read('user.User.id');
+			if ($this->Commentaire->save($this->data)) {
+				$this->redirect('/deliberations/traiter/'.$this->data['Commentaire']['delib_id']);
+			} else {
+				$this->Session->setFlash('Please correct errors below.');
+			}
+		}
+	}
+	
+	
+	
+	function delete($id = null,$delib_id) {
+		if (!$id) {
+			$this->Session->setFlash('Invalid id for Commentaire');
+			$this->redirect('/deliberations/traiter/'.$delib_id);
+		}
+		if ($this->Commentaire->del($id)) {
+			$this->redirect('/deliberations/traiter/'.$delib_id);
+		}
+	}
+	
+	
+	function view($id = null,$delib_id=null) {
+		if (!$id) {
+			$this->Session->setFlash('Invalid id for Commentaire.');
+			$this->redirect('/deliberations/traiter'.$delib_id);
+		}
+		$this->set('commentaire', $this->Commentaire->read(null, $id));
+	}
+	
+}
+?>
