513b7ed9071be289c9c2da12e16d89d2f3abb8d2
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-18 15:12:38 +0000

    Francisation des message setFlash

--- cake_webdelib/controllers/annexes_controller.php
+++ cake_webdelib/controllers/annexes_controller.php
@@ -3,7 +3,7 @@ class AnnexesController extends AppController {
 
 	function delete($id = null) {
 		if (!$id) {
-			$this->Session->setFlash('Mauvais id de commentaire');
+			$this->Session->setFlash('Invalide id pour le commentaire');
 			$this->redirect($action);
 		}
 		if ($this->Annex->del($id)) {
