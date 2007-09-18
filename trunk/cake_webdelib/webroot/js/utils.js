031c3635be07921eff3bbfe3086460ac185c8536
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-18 09:56:50 +0000

    Ajout des labels pour le titre de l'annexe et le chemin

--- trunk/cake_webdelib/webroot/js/utils.js
+++ trunk/cake_webdelib/webroot/js/utils.js
@@ -24,31 +24,39 @@ function add_field() {
  	var d = document.getElementById('cible');
 	var n = d.childNodes.length;
 	var p = document.createElement("p");
+	var br = document.createElement('br');
 	p.id = n;
  	d.appendChild(p);
 
-	
+	var div1 = document.createElement('div');
 	var input1 = document.createElement('input');
 	input1.id = 'AnnexeTitre_'+n;
  	input1.type = 'text';
 	input1.size = '30';
 	input1.name = 'titre_'+n;
-	p.appendChild(input1);
+	var titre = document.createTextNode('Titre annexe');	
+	div1.appendChild(titre);
+	div1.appendChild(input1);
+	p.appendChild(div1);
 
+	var div2 = document.createElement('div');
 	var input2 = document.createElement('input');
 	input2.id = 'AnnexeFile_'+n;
 	input2.type = 'file';
 	input2.size = '40';
 	input2.name = 'file_'+n;
-	p.appendChild(input2);
-	var br = document.createElement('br');
-	p.appendChild(br);
+	var chemin = document.createTextNode('Chemin annexe');	
+	div2.appendChild(chemin);
+	div2.appendChild(br);
+	div2.appendChild(input2);
+	p.appendChild(div2);
   
 	var link = document.createElement('a');
 	link.id = 'Lien_'+n;
 	link.href = 'javascript:del_field('+n+')';
 	text = document.createTextNode('Supprimer');	
 	link.appendChild(text);
+	p.appendChild(br);
 	p.appendChild(link);
 
 	document.getElementById('cible').style.visibility = 'visible';
