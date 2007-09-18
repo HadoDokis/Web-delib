533df91e1bbb1f796f2c479c8bdc45d7d7a66b1c
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-18 14:55:13 +0000

    Francisation des message setFlash

--- cake_webdelib/controllers/typeseances_controller.php
+++ cake_webdelib/controllers/typeseances_controller.php
@@ -11,7 +11,7 @@ class TypeseancesController extends AppController {
 
 	function view($id = null) {
 		if (!$id) {
-			$this->Session->setFlash('Invalid id for Typeseance.');
+			$this->Session->setFlash('Invalide id pour le type de seance.');
 			$this->redirect('/typeseances/index');
 		}
 		$this->set('typeseance', $this->Typeseance->read(null, $id));
@@ -23,10 +23,10 @@ class TypeseancesController extends AppController {
 		} else {
 			$this->cleanUpFields();
 			if ($this->Typeseance->save($this->data)) {
-				$this->Session->setFlash('The Typeseance has been saved');
+				$this->Session->setFlash('Le type de seance a &eacute;t&eacute; sauvegard&eacute;');
 				$this->redirect('/typeseances/index');
 			} else {
-				$this->Session->setFlash('Please correct errors below.');
+				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.');
 			}
 		}
 	}
@@ -34,28 +34,28 @@ class TypeseancesController extends AppController {
 	function edit($id = null) {
 		if (empty($this->data)) {
 			if (!$id) {
-				$this->Session->setFlash('Invalid id for Typeseance');
+				$this->Session->setFlash('Invalide id pour le type de seance');
 				$this->redirect('/typeseances/index');
 			}
 			$this->data = $this->Typeseance->read(null, $id);
 		} else {
 			$this->cleanUpFields();
 			if ($this->Typeseance->save($this->data)) {
-				$this->Session->setFlash('The Typeseance has been saved');
+				$this->Session->setFlash('Le type de seance a &eacute;t&eacute; modifi&eacute;');
 				$this->redirect('/typeseances/index');
 			} else {
-				$this->Session->setFlash('Please correct errors below.');
+				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.');
 			}
 		}
 	}
 
 	function delete($id = null) {
 		if (!$id) {
-			$this->Session->setFlash('Invalid id for Typeseance');
+			$this->Session->setFlash('Invalide id pour le type de seance');
 			$this->redirect('/typeseances/index');
 		}
 		if ($this->Typeseance->del($id)) {
-			$this->Session->setFlash('The Typeseance deleted: id '.$id.'');
+			$this->Session->setFlash('Le type de seance a &eacute;t&eacute; supprim&eacute;');
 			$this->redirect('/typeseances/index');
 		}
 	}
