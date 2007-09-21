7c91d37094762260247ade11ecfa8d71c041728a
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-21 09:18:25 +0000

    AJout du champ position dans la table deliberations afiin de pouvoir classer l'ordre des projets dans une délib.

--- trunk/cake_webdelib/config/sql/patches/deliberations.sql
+++ trunk/cake_webdelib/config/sql/patches/deliberations.sql
@@ -1,2 +1,3 @@
 -- 05/09/2007 : Ajout du champ anterieure_id correspondant à l'id de la version anterieure à la delib
 ALTER TABLE `deliberations` ADD `anterieure_id` INT NOT NULL AFTER `seance_id` ;
+ALTER TABLE `deliberations` ADD `position` INT( 4 ) NOT NULL AFTER `seance_id` ;
