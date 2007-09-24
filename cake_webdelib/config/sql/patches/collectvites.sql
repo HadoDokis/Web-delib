a27fc899e067b3ab715bd980b54a3ed91c81620c
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-24 13:42:30 +0000

    Ajout de la table collectivité

---
+++ cake_webdelib/config/sql/patches/collectvites.sql
@@ -0,0 +1,9 @@
+-- 
+-- Structure de la table `collectivites`
+-- 
+
+CREATE TABLE `collectivites` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
