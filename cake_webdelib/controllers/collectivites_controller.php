e2f573b23b3abf81e73b8a1b9d788be6efe48fe1
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-24 14:14:19 +0000

    Ajout de la possibilité de modifier le logo de la collectivité

--- cake_webdelib/controllers/collectivites_controller.php
+++ cake_webdelib/controllers/collectivites_controller.php
@@ -20,8 +20,27 @@
 		else {
 			if(!empty($this->params['form']))
 				$this->Collectivite->save($this->data);
+		}	
+ 	}
+ 	
+ 	function setLogo($id = null) {
+ 		if (empty($this->data)) {
+			$this->data = $this->Collectivite->read(null, $id);
 		}
- 
+		else {
+		    $type_file = $this->data['Image']['logo']['type'];
+            if( !strstr($type_file, 'jpg') && !strstr($type_file, 'jpeg') && !strstr($type_file, 'bmp') && !strstr($type_file, 'gif') && !strstr($type_file, 'png')){
+                exit("Le fichier n'est pas une image");
+    		}
+			$name_file = 'logo.jpg';
+			$content_dir = '/home/francois/.workspace/svn_webdelib/webroot/files/image/';
+			$tmp_file =  $this->data['Image']['logo']['tmp_name'];
+						
+    		if( !move_uploaded_file($tmp_file, 	$content_dir.$name_file) ){
+       		    exit("Impossible de copier le fichier dans $content_dir");
+   			 }
+			$this->redirect('/collectivites');
+		}
  	}
 }
  
