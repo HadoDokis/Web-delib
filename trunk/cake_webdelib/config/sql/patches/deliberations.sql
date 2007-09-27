42bd3296ebc0dcd17933c7ac5071aed95185f928
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-27 10:13:02 +0000

    modif champ date_limite en type DATE

--- trunk/cake_webdelib/config/sql/patches/deliberations.sql
+++ trunk/cake_webdelib/config/sql/patches/deliberations.sql
@@ -3,4 +3,4 @@ ALTER TABLE `deliberations` ADD `anterieure_id` INT NOT NULL AFTER `seance_id` ;
 ALTER TABLE `deliberations` ADD `position` INT( 4 ) NOT NULL AFTER `seance_id` ;
 
 -- 27/09/2007 : Ajout du champ date_limite
-ALTER TABLE `deliberations` ADD `date_limite` DATETIME NULL AFTER `texte_synthese` ;
+ALTER TABLE `deliberations` ADD `date_limite` DATE NULL AFTER `texte_synthese` ;
