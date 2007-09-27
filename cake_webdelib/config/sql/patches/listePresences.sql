c4d1e165e566a7c9dfd9d9773dff27bceee0fe79
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-27 13:22:21 +0000

    Correction de la structure de la table listePresences.sql

--- cake_webdelib/config/sql/patches/listePresences.sql
+++ cake_webdelib/config/sql/patches/listePresences.sql
@@ -9,6 +9,6 @@ CREATE TABLE `listepresences` (
   `seance_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `present` tinyint(1) NOT NULL,
-  `mandataire` varchar(100) NOT NULL,
+  `mandataire` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 ;
