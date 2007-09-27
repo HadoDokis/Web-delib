0bed19cd281b02cacc1f8e1c6d8ea8cd58c8777a
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-09-27 08:32:03 +0000

    Ajout champ date_limite

--- cake_webdelib/config/sql/patches/deliberations.sql
+++ cake_webdelib/config/sql/patches/deliberations.sql
@@ -1,3 +1,6 @@
 -- 05/09/2007 : Ajout du champ anterieure_id correspondant à l'id de la version anterieure à la delib
 ALTER TABLE `deliberations` ADD `anterieure_id` INT NOT NULL AFTER `seance_id` ;
 ALTER TABLE `deliberations` ADD `position` INT( 4 ) NOT NULL AFTER `seance_id` ;
+
+-- 27/09/2007 : Ajout du champ date_limite
+ALTER TABLE `deliberations` ADD `date_limite` DATETIME NULL AFTER `texte_synthese` ;
