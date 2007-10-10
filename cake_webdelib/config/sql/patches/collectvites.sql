36f18443672758121caca196cfec4dfeb27433da
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-10-10 09:24:29 +0000

    suppression des patch car ils ont été rajouté dans webdelib.sql

--- cake_webdelib/config/sql/patches/collectvites.sql
+++
@@ -1,9 +0,0 @@
--- 
--- Structure de la table `collectivites`
--- 
-
-CREATE TABLE `collectivites` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(30) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
