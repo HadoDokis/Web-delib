11e97e8bb5fa3d5a09f13627a17e9dacf5950098
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-10-17 09:03:33 +0000

    ajout de la colonne "reporte"

--- cake_webdelib/config/sql/patches/deliberations.sql
+++ cake_webdelib/config/sql/patches/deliberations.sql
@@ -1,3 +1,6 @@
 ALTER TABLE `deliberations` ADD `localisation1_id` INT( 11 ) NOT NULL DEFAULT '0' AFTER `etat` ,
 ADD `localisation2_id` INT NOT NULL DEFAULT '0' AFTER `localisation1_id` ,
-ADD `localisation3_id` INT NOT NULL DEFAULT '0' AFTER `localisation2_id`
+ADD `localisation3_id` INT NOT NULL DEFAULT '0' AFTER `localisation2_id`,
+
+-- 17/10/2007
+ADD `reporte` TINYINT( 1 ) NOT NULL AFTER `etat` ;
