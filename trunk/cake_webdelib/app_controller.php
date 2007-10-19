c72a5c11e1d3d26ea911a45e183fd6b0bb697fe9
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-10-19 13:53:38 +0000

    Création dynamique de la navigation en fonction des droits

--- trunk/cake_webdelib/app_controller.php
+++ trunk/cake_webdelib/app_controller.php
@@ -39,32 +39,115 @@
  */
 class AppController extends Controller {
 
-	var $components=array('Utils');
+	var $components=array( 'Utils', 'Acl');
 	var $helpers = array('Html', 'Form' , 'Javascript','Navigation');
-	
+
 	var $beforeFilter = array('checkSession');
 
 	var $infoUser = "";
 	var $lienDeconnexion = "";
 	var $agentServices = null;
-	
-	function checkSession()
-	{
+	var $userProfil = null;
+	var $menu = null;
+
+	function checkSession() {
 		$this->infoUser = "<span class=\"user\">".$this->Session->read('user.User.prenom')." ".$this->Session->read('user.User.nom')."</span> ";
    		$this->agentServices = $this->Session->read('user.Service');
- 	    $this->lienDeconnexion = "[<span class=\"deconnexion\"><a href=\"".$this->base."/users/logout\"> Deconnexion</a></span>]";  
- 	    	  
+ 	    $this->lienDeconnexion = "[<span class=\"deconnexion\"><a href=\"".$this->base."/users/logout\"> Deconnexion</a></span>]";
+
 		if(substr($_SERVER['REQUEST_URI'], strlen($this->base)) != '/users/login')
 		{
 			//s'il n'y a pas d'utilisateur connectÃ© en session
-			if (!$this->Session->Check('user'))
-			{
+			if (!$this->Session->Check('user')) {
 				//le forcer a se connecter
 				$this->redirect('/users/login');
 				exit();
 			}
+			else {
+				$user_id = $this->Session->read('user.User.id');
+       			$aco = $this->name.':'.$this->action;
+        		if ($this->Acl->check($user_id, $aco)){
+          		 	$this->menu = $this->buildNavigation($user_id);
+          		    return;
+        		}
+                else {
+                   // $this->redirect('/users/logout');
+                    die("accès refusé pour $user_id (".$this->Session->read('user.User.prenom')." ".$this->Session->read('user.User.nom').") à $aco");
+                }
+            }
 		}
-		
-	}	
+	}
+
+	function buildNavigation ($user_id){
+		$menu = array( );
+		// construction navigation secondaire projets
+        $sub_menu1 = array ();
+        $sub_menu2 = array ();
+        $sub_menu3 = array ();
+        $sub_menu4 = array ();
+
+		if ($this->Acl->check($user_id, "Deliberations:add")){
+			$sub_menu1['Nouveau...'] = array('link' => '/deliberations/add');
+			$sub_menu1['Mes projets'] = array('link' => '/deliberations/listerMesProjets');
+			$sub_menu1['Non attribués'] = array('link' => '/deliberations/listerProjetsNonAttribues');
+		}
+
+        if ($this->Acl->check($user_id, "Deliberations:listerProjetsATraiter"))
+			$sub_menu1['A traiter'] = array('link' => '/deliberations/listerProjetsATraiter');
+
+        if ($this->Acl->check($user_id, "Deliberations:listerProjetsServicesAssemblees"))
+			$sub_menu1['Projets fin circuit'] = array('link' => '/deliberations/listerProjetsServicesAssemblees');
+
+
+        // construction navigation secondaire seances
+        $sub_menu2 = array (
+        	'Nouvelle...' => array('link' => '/seances/add'),
+        	'A venir' => array ('link' => '/seances/listerFuturesSeances'),
+        	'Passées' => array('link' => '/seances/listerAnciennesSeances'),
+        	'Calendrier' => array('link' => '/seances/afficherCalendrier')
+        );
+
+        // construction navigation secondaire post-seance
+        $sub_menu3 = array (
+        	'Editions' => array('link' => '/postseances/index'),
+        	//'Publications' => array('link' => '/seances/listerAnciennesSeances'),
+        	'Contrôle de légalité' => array('link' => '/deliberations/transmit'),
+        	'Export GED' => array('link' => '/pages/exportged')
+        );
+
+        // construction navigation secondaire administration
+        $sub_menu4 = array (
+        	'Utilisateurs' => array('link' => '/users/index'),
+        	'Circuits' => array('link' => '/circuits/index'),
+        	'Profils' => array('link' => '/profils/index'),
+	        'Service' => array('link' => '/services/index'),
+	        'Thèmes' => array('link' => '/themes/index'),
+	        'Types de séance' => array('link' => '/typeseances'),
+	        'Collectivité' => array('link' => '/collectivites'),
+	        'Génération' => array('link' => '/models/index'),
+	        'Localisation' => array('link' => '/localisations/index')
+        );
+
+		// construction navigation principale
+		if ($this->Acl->check($user_id, "Pages"))
+			$menu['Accueil']= array('link' => '/');
+
+		if ($this->Acl->check($user_id, "Deliberations:index")){
+			$menu['Projets']= array('link' => '/deliberations/listerMesProjets', 'submenu' => array());
+			$menu['Projets']['submenu'] = $sub_menu1;
+		}
+		if ($this->Acl->check($user_id, "Seances:index")){
+			$menu['Seances']= array('link' => '/seances/', 'submenu' => array());
+			$menu['Seances']['submenu'] = $sub_menu2;
+			$menu['Post-seance']= array('link' => '/pages/postseance', 'submenu' => array());
+			$menu['Post-seance']['submenu'] = $sub_menu3;
+		}
+
+		if ($this->Acl->check($user_id, "Pages:administration")){
+			$menu['Administration']= array('link' => '/pages/administration', 'submenu' => array());
+			$menu['Administration']['submenu'] = $sub_menu4;
+		}
+        return $menu;
+	}
 }
 ?>
