8968d794758c0c910c9aca9b4339314344ede0ca
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-10-19 13:43:59 +0000

    Mise en place du plugin acm

---
+++ trunk/cake_webdelib/plugins/acm/config.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Yes. :  It's That Easy, Period <http://www.yesperiod.org/>
+ * Copyright (c) 2006, Yes Period, Inc.
+ *                     1610 Forster Street
+ *                     Harrisburg, PA 17103
+ *
+ * @filesource   config.php
+ * @copyright    Copyright (c) 2006, Yes Period, Inc.
+ * @link         http://www.yesperiod.com/
+ * @package      ACM
+ * @modifiedby   $LastChangedBy: Ryan J. Peterson $
+ * @lastmodified $Date: 2006/06/22 10:22:24 $
+ */
+
+ /* ------ ACM AutoLoad Config Vars ----- */
+
+ /* This sets AutoLoad on or off
+  * using true or false. This is to
+  * connect ACM to your existing Users/Groups
+  */
+	define('ACMAUTOLOAD', false);
+
+ /* This defines the tables that hold
+  * your existing Users/Groups.
+  */
+	define('ACMROLES','profils');
+	define('ACMUSERS','users');
+
+ /* This defines the fields that hold
+  * the Users/Groups unique alias.
+  */
+	define('ACMROLE_ALIAS','libelle');
+	define('ACMUSER_ALIAS','login');
+
+	/* this constant allows you to decide
+	 * if ACM can add Users & Groups additional
+	 * to those managed by AutoLoad, defaults
+	 * to true if AutoLoad is off.
+	 */
+	define('ALLOWADDACO', true);
+
+/* -------------------------------------- */
+
+ ?>
