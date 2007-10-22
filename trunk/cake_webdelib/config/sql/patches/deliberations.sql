9b6f9947bfc2231bb27d7ce39ea28f509fa53b69
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-10-22 13:07:08 +0000

    ajout du champ reporte limitation des champs titre et objet a 1000 caracteres

--- trunk/cake_webdelib/config/sql/patches/deliberations.sql
+++ trunk/cake_webdelib/config/sql/patches/deliberations.sql
@@ -1,6 +1,6 @@
-ALTER TABLE `deliberations` ADD `localisation1_id` INT( 11 ) NOT NULL DEFAULT '0' AFTER `etat` ,
-ADD `localisation2_id` INT NOT NULL DEFAULT '0' AFTER `localisation1_id` ,
-ADD `localisation3_id` INT NOT NULL DEFAULT '0' AFTER `localisation2_id`,
-
 -- 17/10/2007
-ADD `reporte` TINYINT( 1 ) NOT NULL AFTER `etat` ;
+ALTER TABLE `deliberations` ADD `reporte` TINYINT( 1 ) NOT NULL AFTER `etat`,
+
+-- 22/10/2007
+CHANGE `objet` `objet` VARCHAR( 1000 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
+CHANGE `titre` `titre` VARCHAR( 1000 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL 
