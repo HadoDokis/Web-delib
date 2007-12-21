296d04ad2cc7ad4265913c2fac414ad4b6faf5dc
marinepontis <marinepontis@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2007-12-21 14:11:52 +0000

    la liste des participants se fait delib par delib et non plus par seances

--- trunk/cake_webdelib/controllers/postseances_controller.php
+++ trunk/cake_webdelib/controllers/postseances_controller.php
@@ -35,14 +35,20 @@ class PostseancesController extends AppController {
 	function getVote($id_delib){
 		$condition = "delib_id = $id_delib";
 		$votes = $this->Vote->findAll($condition);
-		if (!empty($votes)){$resultat =$votes[0]['Vote']['commentaire'];
-		return $resultat;}
+		if (!empty($votes)){
+			$resultat =$votes[0]['Vote']['commentaire'];
+			return $resultat;
+		}
+	}
+	
+	function getPresence($id_delib,$present){
+		$condition ="delib_id =$id_delib AND present=$present";
+		$presences = $this->Listepresence->findAll($condition);
+		return $presences;
 	}
 	
 	function generatePvSommaire ($id=null) {
 		$delib=array();
-		$this->set('presents', $this->Listepresence->findAll("seance_id =$id AND present=1"));
-		$this->set('absents', $this->Listepresence->findAll("seance_id =$id AND present=0"));
 		$seance = $this->Seance->findAll("Seance.id = $id");
 		$this->set('seance',$seance);
 		$this->set('model',$this->Model->findAll());
@@ -56,6 +62,8 @@ class PostseancesController extends AppController {
 		{
 			$projId = $proj['Deliberation']['id'];
 			$proj['vote']= $this->getVote($projId);
+			$proj['present']=$this->getPresence($projId,1);
+			$proj['absent']=$this->getPresence($projId,0);
 			array_push($delib, $proj);	
 		}
 		$this->set('projets',$delib);	
@@ -63,12 +71,9 @@ class PostseancesController extends AppController {
 
 	function generatePvComplet ($id=null) {
 		$delib=array();
-		$this->set('presents', $this->Listepresence->findAll("seance_id =$id AND present=1"));
-		$this->set('absents', $this->Listepresence->findAll("seance_id =$id AND present=0"));
 		$seance = $this->Seance->findAll("Seance.id = $id");
 		$this->set('seance',$seance);
 		$this->set('model',$this->Model->findAll());
-		//$this->set('themes',$this->Theme->findAll());
 		$condition= "seance_id=$id AND etat>=2";
 		$projets =  $this->Deliberation->findAll($condition,null,'position ASC');
 		$this->set('jour', $this->Date->days[intval(date('w'))]);
@@ -79,6 +84,8 @@ class PostseancesController extends AppController {
 		{
 			$projId = $proj['Deliberation']['id'];
 			$proj['vote']= $this->getVote($projId);
+			$proj['present']=$this->getPresence($projId,1);
+			$proj['absent']=$this->getPresence($projId,0);
 			array_push($delib, $proj);	
 		}
 		$this->set('projets',$delib);	
@@ -94,10 +101,13 @@ class PostseancesController extends AppController {
 		$this->set('mois', $this->Date->months[intval(date('m'))]);
 		$this->set('collectivite',  $this->Collectivite->findAll());
 		$this->set('date_seance',  $this->Date->frenchDateConvocation(strtotime($projet[0]['Seance']['date'])));
-		//debug($projet);
-		$this->set('votespour', $this->Vote->findAll("delib_id=$id AND resultat=1"));
-		$this->set('abstenus', $this->Vote->findAll("delib_id=$id AND resultat in (2,3)"));
+		
+		$this->set('votespour', $this->Vote->findAll("delib_id=$id AND resultat=3"));
+		$this->set('votescontre', $this->Vote->findAll("delib_id=$id AND resultat=2"));
+		$this->set('abstenus', $this->Vote->findAll("delib_id=$id AND resultat=4"));
+		$this->set('sans_part', $this->Vote->findAll("delib_id=$id AND resultat=5"));
 		$this->set('vote',$this->Vote->findAll("delib_id=$id"));
+		$this->set('absents', $this->Listepresence->findAll("delib_id =$id AND present=0"));
 		$this->set('dl', $dl);
 	}
 
@@ -105,7 +115,7 @@ class PostseancesController extends AppController {
 	function getNom($id)
 	{
 		$data = $this->User->findAll("User.id = $id");
-		return $data['0']['User']['nom'].' '.$data['0']['User']['prenom'];
+		return $data['0']['User']['prenom'].' '.$data['0']['User']['nom'];
 	}
 
 }
