ec8bcf37eb654d1232634b56b9db73eb0dcce0d8
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2008-01-11 10:17:43 +0000

    Prévision du multi-databases avec CAKEPHP

--- cake_webdelib/config/database.php
+++ cake_webdelib/config/database.php
@@ -1,4 +1,14 @@
 <?php
+
+// On prend du premier caractere de l'url jusqu'au premier .
+// comme nom de la base de données.
+//
+//ex. : http://collectivite.webdelib.demonstrations.adullact.org
+// 		DATABASE = collectivite
+//
+
+define ('DATABASE', substr($_SERVER['SERVER_NAME'], 0, strpos ($_SERVER['SERVER_NAME'], '.')));
+
 class DATABASE_CONFIG {
 
 	var $default = array(
@@ -7,8 +17,10 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'webdelib', 
-		'prefix' => '' 
+//		'database' => DATABASE,
+		'database' => 'webdelib',
+		'prefix' => ''
 	);
 }
+
 ?>
