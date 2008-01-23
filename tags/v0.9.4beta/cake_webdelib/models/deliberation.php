d070700402489ce440d47cc551338a7b411be16c
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2008-01-23 14:31:49 +0000

    Etiquetage de la version 0.9.4 beta

---
+++ tags/v0.9.4beta/cake_webdelib/models/deliberation.php
@@ -0,0 +1,85 @@
+<?php
+class Deliberation extends AppModel {
+
+	var $name = 'Deliberation';
+	var	$cacheQueries = false;
+	//dependent : pour les suppression en cascades. ici à false pour ne pas modifier le referentiel
+	var $belongsTo=array('Service'=>array('className'=>'Service',
+											'conditions'=>'',
+											'order'=>'',
+											'dependent'=>false,
+											'foreignKey'=>'service_id'),
+						'Theme'=>array('className'=>'Theme',
+											'conditions'=>'',
+											'order'=>'',
+											'dependent'=>false,
+											'foreignKey'=>'theme_id'),
+						'Circuit'=>array('className'=>'Circuit',
+											'conditions'=>'',
+											'order'=>'',
+											'dependent'=>false,
+											'foreignKey'=>'circuit_id'),
+						'Redacteur' =>array('className'    => 'User',
+                              'conditions'   => '',
+                              'order'        => '',
+                              'dependent'    =>  true,
+                              'foreignKey'   => 'redacteur_id'
+                        ),
+                        'Rapporteur'=> array('className'    => 'User',
+                              'conditions'   => '',
+                              'order'        => '',
+                              'dependent'    =>  true,
+                              'foreignKey'   => 'rapporteur_id'),
+                         'Seance'=> array('className'    => 'Seance',
+                              'conditions'   => '',
+                              'order'        => '',
+                              'dependent'    =>  true,
+                              'foreignKey'   => 'seance_id'),
+                         'Localisation'=> array('className'    => 'Localisation',
+                              'conditions'   => '',
+                              'order'        => '',
+                              'dependent'    =>  true,
+                              'foreignKey'   => 'localisation1_id')
+						);
+	var $hasMany = array ('Traitement' => array('className'=>'Traitement',
+												'foreignKey' => 'delib_id'),
+							'Annexe'=>array('className'=>'Annex',
+											'foreignKey'=>'deliberation_id'),
+							'Commentaire' => array('className' => 'Commentaire',
+											'foreignKey' => 'delib_id'));
+//var $hasMany = 'Annex';
+
+
+//
+//	var $hasOne = array('Redacteur' =>
+//                        array('className'    => 'User',
+//                              'conditions'   => '',
+//                              'order'        => '',
+//                              'dependent'    =>  true,
+//                              'foreignKey'   => 'redacteur_id'
+//                        ),
+//                              'Rapporteur'=>
+//                        array('className'    => 'User',
+//                              'conditions'   => '',
+//                              'order'        => '',
+//                              'dependent'    =>  true,
+//                              'foreignKey'   => 'rapporteur_id')
+//
+//                  );
+//
+
+//	var $hasAndBelongsToMany=array('User'=>array('classname'=>'User',
+//													'joinTable'=>'users_profils',
+//													'foreignKey'=>'user_id',
+//													'associationForeignKey'=>'profil_id',
+//													'conditions'=>'',
+//													'order'=>'',
+//													'limit'=>'',
+//													'unique'=>true,
+//													'finderQuery'=>'',
+//													'deleteQuery'=>''));
+
+
+
+}
+?>
---
+++ tags/v0.9.4beta/cake_webdelib/views/helpers/navigation.php
@@ -0,0 +1,67 @@
+<?php
+class NavigationHelper extends HtmlHelper
+{
+    var $tab = "\t";
+     
+	function buildMenu($links = array(), $ulId = "", $ulClass = "displayed", $level = 0)
+    {   
+    	$ulTab = str_repeat($this->tab, $level);
+    	$liTab = str_repeat($this->tab, $level+1);
+		$cpt = 0;
+		$tmp = "";
+
+		$out = $ulTab."<ul id=\"$ulId\" class=\"$ulClass\">\n";
+        foreach ($links as $title => $data)
+        {	
+        	if(($this->url($data['link']) == $this->here) || (!empty($data['submenu']) && $this->submenu_array_contains($data['submenu'], $this->here)))
+			{
+				$liClass = "here";
+				$ulClass = "displayed";
+			}
+			else
+			{
+				$liClass = "";
+				$ulClass = "hidden";
+			}
+
+        	$tmp .= $liTab."<li class=\"$liClass\">".$this->link($title,$data['link']);
+			if(empty($data['submenu']))
+			{
+				$tmp .= "</li>\n";
+			}
+			else
+			{
+				$tmp .= "\n".$this->buildMenu($data['submenu'],"subnav-$cpt", $ulClass, $level+2);
+				$tmp .= $liTab."</li>\n";
+				
+			}
+			$cpt++;
+        }
+        
+        $out .= $tmp;
+        $out .= $ulTab."</ul>\n";
+	
+	if ($ulClass== "displayed")
+	{
+		$out="<div class=\"bloc_menu\">\n".$out."</div>\n";
+	}
+	//debug($out);
+	//exit;
+        return $out;
+    }
+    
+    function submenu_array_contains($submenu_array, $value)
+    {
+        $links = array_values($submenu_array);
+        $contains = false;
+        foreach($links as $link) 
+        {
+            if($this->url($link['link']) == $value)
+            {
+                $contains = true;
+            }
+        }
+        return $contains;
+    }    
+}
+?>
