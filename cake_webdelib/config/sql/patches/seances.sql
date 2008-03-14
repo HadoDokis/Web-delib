9399ca792896ddf30452ca2b32d94a0a223c706f
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2008-03-14 13:37:11 +0000

    Ajout du patch sql pour que la nouvelle fonction du controller deliberations PositionnerDelibsSeances() soit intégrer aux acms

--- cake_webdelib/config/sql/patches/seances.sql
+++ cake_webdelib/config/sql/patches/seances.sql
@@ -16,3 +16,9 @@ INSERT INTO `webdelib`.`aros_acos` (
 VALUES (
 NULL , '3', '268', '1', '1', '1', '1'
 );
+
+
+INSERT INTO `acos` (`id`, `object_id`, `alias`, `lft`, `rght`) VALUES
+(268, 0, 'Deliberations:PositionneDelibsSeance', 522, 523);
+INSERT INTO `aros_acos` (`id`, `aro_id`, `aco_id`, `_create`, `_read`, `_update`, `_delete`) VALUES
+(null, 3, 269, '1', '1', '1', '1');
