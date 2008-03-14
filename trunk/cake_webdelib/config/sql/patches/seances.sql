cb2b364166fdb10089e8434e050d9bafceb706a1
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2008-03-14 10:28:30 +0000

    Ajout de la fonction changeStatus dans la table acos

--- trunk/cake_webdelib/config/sql/patches/seances.sql
+++ trunk/cake_webdelib/config/sql/patches/seances.sql
@@ -1,1 +1,18 @@
 ALTER TABLE `seances` ADD `traitee` INT( 1 ) NOT NULL DEFAULT '0';
+
+INSERT INTO `acos` (`id`, `object_id`, `alias`, `lft`, `rght`) VALUES
+(268, 0, 'Seances:changeStatus', 520, 521);
+
+INSERT INTO
+INSERT INTO `webdelib`.`aros_acos` (
+`id` ,
+`aro_id` ,
+`aco_id` ,
+`_create` ,
+`_read` ,
+`_update` ,
+`_delete`
+)
+VALUES (
+NULL , '3', '268', '1', '1', '1', '1'
+);
