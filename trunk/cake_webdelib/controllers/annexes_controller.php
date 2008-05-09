7f7efe29b495f895426e8654f1bd797992786be5
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2008-05-09 07:47:38 +0000

    Redirection vers '/' en cas d'id invalide pour la fonction delete

--- trunk/cake_webdelib/controllers/annexes_controller.php
+++ trunk/cake_webdelib/controllers/annexes_controller.php
@@ -7,7 +7,7 @@ class AnnexesController extends AppController {
 	function delete($id = null) {
 		if (!$id) {
 			$this->Session->setFlash('Invalide id pour le commentaire');
-			$this->redirect($action);
+			$this->redirect('/');
 		}
 		if ($this->Annex->del($id)) {
 			$this->redirect($this->Session->read('user.User.lasturl'));
