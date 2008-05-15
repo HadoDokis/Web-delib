5c0d024e6885dc673251733c4d70f4337e8dc2af
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2008-05-15 07:20:22 +0000

    Mise en correspondance du model par rapport à la table en base et au fonctionnel

--- cake_webdelib/models/vote.php
+++ cake_webdelib/models/vote.php
@@ -2,11 +2,6 @@
 class Vote extends AppModel {
 
 	var $name = 'Vote';
-	var $validate = array(
-		'liste_id' => VALID_NOT_EMPTY,
-		'seance_id' => VALID_NOT_EMPTY,
-		'delib_id' => VALID_NOT_EMPTY,
-	);
 
 }
 ?>
