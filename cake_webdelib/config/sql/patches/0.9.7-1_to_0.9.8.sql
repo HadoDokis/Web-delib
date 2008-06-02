ba3ca7666153f0b9e2090c9e423d12a5e82c25f6
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2008-06-02 15:19:02 +0000

    Ajout du patch pour l'ajout des modèles à générer avec GEDOOo.

---
+++ cake_webdelib/config/sql/patches/0.9.7-1_to_0.9.8.sql
@@ -0,0 +1,13 @@
+ALTER TABLE `models` ADD `name` VARCHAR( 255 ) NULL ;
+ALTER TABLE `models` ADD `size` INT( 11 ) NOT NULL ;
+ALTER TABLE `models` ADD `extension`  VARCHAR( 255 ) NULL ;
+ALTER TABLE `models` CHANGE `texte` `content` LONGBLOB NOT NULL;
+ALTER TABLE `deliberations` ADD `texte_projet_size` INT( 11 ) NOT NULL ;
+ALTER TABLE `deliberations` ADD `texte_projet_type`  VARCHAR( 255 ) NULL ;
+ALTER TABLE `deliberations` ADD `texte_projet_name`  VARCHAR( 255 ) NULL ;
+ALTER TABLE `deliberations` ADD `texte_synthese_name`  VARCHAR( 255 ) NULL ;
+ALTER TABLE `deliberations` ADD `texte_synthese_size` INT( 11 ) NOT NULL ;
+ALTER TABLE `deliberations` ADD `texte_synthese_type`  VARCHAR( 255 ) NULL ;
+ALTER TABLE `deliberations` ADD `deliberation_size` INT( 11 ) NOT NULL ;
+ALTER TABLE `deliberations` ADD `deliberation_type`  VARCHAR( 255 ) NULL ;
+ALTER TABLE `deliberations` ADD `deliberation_name`  VARCHAR( 255 ) NULL ;
