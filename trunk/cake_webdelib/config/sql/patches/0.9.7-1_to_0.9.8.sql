6450f960109f386feea9b45ece96f74432ac0796
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2008-06-09 09:33:26 +0000

    Ajout du champs 'modele' dans la table 'modele'

--- trunk/cake_webdelib/config/sql/patches/0.9.7-1_to_0.9.8.sql
+++ trunk/cake_webdelib/config/sql/patches/0.9.7-1_to_0.9.8.sql
@@ -2,6 +2,9 @@ ALTER TABLE `models` ADD `name` VARCHAR( 255 ) NULL ;
 ALTER TABLE `models` ADD `size` INT( 11 ) NOT NULL ;
 ALTER TABLE `models` ADD `extension`  VARCHAR( 255 ) NULL ;
 ALTER TABLE `models` CHANGE `texte` `content` LONGBLOB NOT NULL;
+
+ALTER TABLE `models` ADD `modele` VARCHAR( 50 ) NOT NULL AFTER `id` ;
+
 ALTER TABLE `deliberations` ADD `texte_projet_size` INT( 11 ) NOT NULL ;
 ALTER TABLE `deliberations` ADD `texte_projet_type`  VARCHAR( 255 ) NULL ;
 ALTER TABLE `deliberations` ADD `texte_projet_name`  VARCHAR( 255 ) NULL ;
