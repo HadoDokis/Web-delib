8ec02dd7249b559b9ea02c76fb0ae6a5235cacd9
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2008-06-17 09:48:19 +0000

    Nouvelle gestion des acteurs

---
+++ cake_webdelib/controllers/typeacteurs_controller.php
@@ -0,0 +1,86 @@
+<?php
+class TypeacteursController extends AppController
+{
+	var $name = 'Typeacteurs';
+
+	// Gestion des droits : identiques aux droits des acteurs
+	var $commeDroit = array(
+		'index' => 'Pages:acteurs_typeacteurs',
+		'add' => 'Pages:acteurs_typeacteurs',
+		'edit' => 'Pages:acteurs_typeacteurs',
+		'delete' => 'Pages:acteurs_typeacteurs',
+		'view' => 'Pages:acteurs_typeacteurs'
+		);
+
+	function index()
+	{
+		$this->set('typeacteurs', $this->Typeacteur->findAll());
+	}
+
+	function view($id = null)
+	{
+		if (!$this->Typeacteur->exists()) {
+			$this->Session->setFlash('Invalide id pour le type d\'acteur');
+			$this->redirect('/pages/acteurs_typeacteurs');
+		} else
+			$this->set('typeacteur', $this->Typeacteur->read(null, $id));
+	}
+
+	function add() {
+		$sortie = false;
+		if (!empty($this->data)) {
+			$this->cleanUpFields();
+			if ($this->Typeacteur->save($this->data)) {
+				$this->Session->setFlash('Le type d\'acteur \''.$this->data['Typeacteur']['nom'].'\' a &eacute;t&eacute; ajout&eacute;');
+				$sortie = true;
+			} else
+				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.');
+		}
+		if ($sortie)
+			$this->redirect('/pages/acteurs_typeacteurs');
+		else {
+			$this->set('eluNonElu',array('1'=>'élu','0'=>'non élu'));
+			$this->render('edit');
+		}
+	}
+
+	function edit($id = null) {
+		$sortie = false;
+		if (empty($this->data)) {
+			$this->data = $this->Typeacteur->read(null, $id);
+			if (empty($this->data)) {
+				$this->Session->setFlash('Invalide id pour le type d\'acteur');
+				$sortie = true;
+			}
+		} else {
+			$this->cleanUpFields();
+			if ($this->Typeacteur->save($this->data)) {
+				$this->Session->setFlash('Le type d\'acteur \''.$this->data['Typeacteur']['nom'].'\' a &eacute;t&eacute; modifi&eacute;');
+				$sortie = true;
+			} else
+				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.');
+		}
+		if ($sortie)
+			$this->redirect('/pages/acteurs_typeacteurs');
+		else
+			$this->set('eluNonElu',array('1'=>'élu','0'=>'non élu'));
+	}
+
+	function delete($id = null) {
+		$typeacteur = $this->Typeacteur->read('id, nom', $id);
+		if (empty($typeacteur)) {
+			$this->Session->setFlash('Invalide id pour le type d\'acteur');
+			$this->redirect('/pages/acteurs_typeacteurs');
+		}
+		elseif (!empty($typeacteur['Acteur'])) {
+			$this->Session->setFlash('Le type d\'acteur \''.$typeacteur['Typeacteur']['nom'].'\' est utilis&eacute; par un acteur. Suppression impossible.');
+			$this->redirect('/pages/acteurs_typeacteurs');
+		}
+		elseif ($this->Typeacteur->del($id)) {
+			$this->Session->setFlash('Le type d\'acteur \''.$typeacteur['Typeacteur']['nom'].'\' a &eacute;t&eacute; supprim&eacute;');
+			$this->redirect('/pages/acteurs_typeacteurs');
+		}
+	}
+
+}
+?>
