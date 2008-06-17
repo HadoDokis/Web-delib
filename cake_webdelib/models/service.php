4a353e40a326ecfd7275b402a73053b4d8631fa9
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2008-06-17 09:30:45 +0000

    Nouvelle gestion des users

--- cake_webdelib/models/service.php
+++ cake_webdelib/models/service.php
@@ -6,7 +6,31 @@ class Service extends AppModel {
 	var $validate = array(
 		'libelle' => VALID_NOT_EMPTY,
 	);
-	var $hasMany = 'User';
+
+	var $hasAndBelongsToMany = array(
+		'User' => array(
+			'classname'=>'User',
+			'joinTable'=>'users_services',
+			'foreignKey'=>'service_id',
+			'associationForeignKey'=>'user_id',
+			'conditions'=>'',
+			'order'=>'',
+			'limit'=>'',
+			'unique'=>true,
+			'finderQuery'=>'',
+			'deleteQuery'=>''),
+		'Acteur' => array(
+			'classname'=>'Acteur',
+			'joinTable'=>'acteurs_services',
+			'foreignKey'=>'service_id',
+			'associationForeignKey'=>'acteur_id',
+			'conditions'=>'',
+			'order'=>'',
+			'limit'=>'',
+			'unique'=>true,
+			'finderQuery'=>'',
+			'deleteQuery'=>'')
+	);
 
 }
 ?>
