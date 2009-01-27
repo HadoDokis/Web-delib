5b68cb6ee7ed033b7b449d6b95b6e131151f9997
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2009-01-27 10:10:16 +0000

    Mise à jour de l'encodage des fichiers en ISO-8859-15

--- trunk/cake_webdelib/models/seance.php
+++ trunk/cake_webdelib/models/seance.php
@@ -3,8 +3,8 @@ class Seance extends AppModel {
 
     var $name = 'Seance';
     var $days = array ('Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi');
-    var $months = array ('','Janvier','FÃ©vrier','Mars','Avril','Mai','Juin',
-			 'Juillet','AoÃ»t','Septembre','Octobre','Novembre','DÃ©cembre');
+    var $months = array ('','Janvier','Février','Mars','Avril','Mai','Juin',
+			 'Juillet','Août','Septembre','Octobre','Novembre','Décembre');
 
     var	$cacheQueries = false;
 
@@ -28,12 +28,12 @@ class Seance extends AppModel {
         'foreignKey' => 'secretaire_id')
     );
 
-     /* retourne la liste des sÃ©ances futures avec le nom du type de sÃ©ance  */
+     /* retourne la liste des séances futures avec le nom du type de séance  */
      function generateList() {
          $generateList = array();
          $conditions= 'Seance.traitee = 0';
          $seances = $this->findAll($conditions, null, 'date ASC');
-	 foreach ($seances as $seance){
+         foreach ($seances as $seance){
 	     $retard=$seance['Typeseance']['retard'];
              if($seance['Seance']['date'] >=date("Y-m-d", mktime(date("H"), date("i"), date("s"), date("m"), date("d")+$retard,  date("Y")))){
 	         $dateTimeStamp = strtotime($seance['Seance']['date']); 
