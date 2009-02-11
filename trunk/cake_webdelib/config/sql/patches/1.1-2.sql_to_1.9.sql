3c3e843e522395f55cdabde756b77e5f8316d2a0
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2009-02-11 17:13:13 +0000

    MAJ de la structure de la base

--- trunk/cake_webdelib/config/sql/patches/1.1-2.sql_to_1.9.sql
+++ trunk/cake_webdelib/config/sql/patches/1.1-2.sql_to_1.9.sql
@@ -2,3 +2,7 @@ ALTER TABLE `deliberations` ADD `commission` LONGBLOB NOT NULL ,
 ADD `commission_size` INT( 11 ) NOT NULL ,
 ADD `commission_type` VARCHAR( 255 ) NOT NULL ,
 ADD `commission_name` VARCHAR( 255 ) NOT NULL ;
+
+ALTER TABLE `deliberations` ADD `debat_name` VARCHAR( 255 ) NOT NULL AFTER `debat` ,
+ADD `debat_size` INT( 11 ) NOT NULL AFTER `debat_name` ,
+ADD `debat_type` VARCHAR( 255 ) NOT NULL AFTER `debat_size` ;
