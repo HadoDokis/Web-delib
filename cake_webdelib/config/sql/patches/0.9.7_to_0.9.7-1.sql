fcf0825f8e9fd239f09a461c411bc793efd65d71
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2009-03-06 13:53:44 +0000

    Suppression des patch pre V1.0

--- cake_webdelib/config/sql/patches/0.9.7_to_0.9.7-1.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `users` CHANGE `teldom` `teldom` VARCHAR( 20 ) NULL DEFAULT
-NULL ,
-CHANGE `telmobile` `telmobile` VARCHAR( 20 ) NULL DEFAULT NULL;
-
-ALTER TABLE `collectivites` CHANGE `telephone` `telephone` VARCHAR( 20 )
-NOT NULL;
