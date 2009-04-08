4dc9a707e04310076c14004a8db3bacf2d209f47
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2009-04-08 09:52:07 +0000

    Mise à jour des patchs sql pour la v2

--- trunk/cake_webdelib/config/sql/patches/1.1_to_1.1-2.sql
+++ trunk/cake_webdelib/config/sql/patches/1.1_to_1.1-2.sql
@@ -1,9 +1,2 @@
-ALTER TABLE `seances` ADD `debat_global_name` VARCHAR( 75 ) NOT NULL ,
-ADD `debat_global_size` INT( 11 ) NOT NULL ,
-ADD `debat_global_type` VARCHAR( 255 ) NOT NULL ;
-
-INSERT INTO `models` (`id`, `modele`, `type`, `name`, `size`, `extension`, `content`) VALUES
-(15, 'Liste abstenants', 'Composant documentaire', NULL, 0, NULL, 0x235052454e4f4d5f41425354454e414e542320234e4f4d5f41425354454e414e54232d2d);
-
 INSERT INTO `models` (`id` ,`modele` ,`type` ,`name` ,`size` ,`extension`) VALUES
 ('9999', 'Liste Projets Ordre du Jour', 'Composant documentaire', '', '', '');
