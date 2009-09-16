371f8b447111f9abf28e74edbca4b14ba8cc2237
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2009-09-16 15:13:29 +0000

    Dépôt initial du patch sql de la v2.0 à la V2.1

---
+++ cake_webdelib/config/sql/patches/2.0.sql_to_2.1.sql
@@ -0,0 +1,6 @@
+-- --------------------------------------------------------
+--
+-- Modification de la table `infosupdefs`
+--
+
+ALTER TABLE `infosupdefs` ADD `val_initiale` VARCHAR( 255 ) NULL AFTER `type` ;
