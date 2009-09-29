897c0e7f0d28fdefac8e3ad9b7b6ad165ed3f4f4
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2009-09-29 08:21:12 +0000

    Changement de la structure de la table deliberations, on ajoute la date d'accusé de reception et l'id renvoyée par le Tdt s2LOW

--- cake_webdelib/config/sql/patches/2.0.sql_to_2.1.sql
+++ cake_webdelib/config/sql/patches/2.0.sql_to_2.1.sql
@@ -4,3 +4,11 @@
 --
 
 ALTER TABLE `infosupdefs` ADD `val_initiale` VARCHAR( 255 ) NULL AFTER `type` ;
+
+-- --------------------------------------------------------
+--
+-- Modification de la table `deliberations`
+--
+
+ALTER TABLE `deliberations` ADD `tdt_id` INT( 11 ) NULL AFTER `num_pref` ,
+ADD `dateAR` VARCHAR( 100 ) NULL AFTER `tdt_id` 
