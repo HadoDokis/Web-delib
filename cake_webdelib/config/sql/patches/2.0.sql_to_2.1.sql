56f52b628d207a6a927644f159a69e07b65feac1
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2009-11-02 15:36:39 +0000

    Changement des droits en fonction des profils pour simplifier les tests Selenium

--- cake_webdelib/config/sql/patches/2.0.sql_to_2.1.sql
+++ cake_webdelib/config/sql/patches/2.0.sql_to_2.1.sql
@@ -2,14 +2,12 @@
 --
 -- Modification de la table `infosupdefs`
 --
-
 ALTER TABLE `infosupdefs` ADD `val_initiale` VARCHAR( 255 ) NULL AFTER `type` ;
 
 -- --------------------------------------------------------
 --
 -- Modification de la table `deliberations`
 --
-
 ALTER TABLE `deliberations` ADD `tdt_id` INT( 11 ) NULL AFTER `num_pref` ,
 ADD `dateAR` VARCHAR( 100 ) NULL AFTER `tdt_id` ;
 
@@ -19,6 +17,15 @@ UPDATE `aros_acos` SET `_create` = '1',
 `_update` = '1',
 `_delete` = '1' WHERE `aros_acos`.`id` =31 LIMIT 1 ;
 
+UPDATE `aros_acos` SET `_create` = '1',
+`_read` = '1',
+`_update` = '1',
+`_delete` = '1' WHERE `aros_acos`.`id` =32 LIMIT 1 ;
+
+UPDATE `aros_acos` SET `_create` = '1',
+`_read` = '1',
+`_update` = '1',
+`_delete` = '1' WHERE `aros_acos`.`id` = 29 LIMIT 1 ;
 
 UPDATE `aros_acos` SET `_create` = '1',
 `_read` = '1',
