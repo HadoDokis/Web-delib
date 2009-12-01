077814d6217c2804a5914948d935e70fbbc6ead8
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2009-12-01 13:04:37 +0000

    On différencie les commentaires ajoutés par un utilisateurs des commentaires ajoutés automatiquement lors des commissions. + Ajout d'un commentaire au séance.

--- trunk/cake_webdelib/config/sql/patches/2.0.sql_to_2.1.sql
+++ trunk/cake_webdelib/config/sql/patches/2.0.sql_to_2.1.sql
@@ -11,7 +11,9 @@ ALTER TABLE `infosupdefs` ADD `val_initiale` VARCHAR( 255 ) NULL AFTER `type` ;
 ALTER TABLE `deliberations` ADD `tdt_id` INT( 11 ) NULL AFTER `num_pref` ,
 ADD `dateAR` VARCHAR( 100 ) NULL AFTER `tdt_id` ;
 
-ALTER TABLE `deliberations` ADD `delib_pdf` LONGBLOB NULL AFTER `vote_commentaire` 
+ALTER TABLE `deliberations` ADD `delib_pdf` LONGBLOB NULL AFTER `vote_commentaire` ;
+ALTER TABLE `seances` ADD `commentaire` VARCHAR( 500 ) NULL AFTER `traitee`;
+ALTER TABLE `commentaires` ADD `commentaire_auto` TINYINT( 1 ) NOT NULL AFTER `pris_en_compte` 
 
 UPDATE `aros_acos` SET `_create` = '1',
 `_read` = '1',
