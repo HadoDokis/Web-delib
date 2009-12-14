965203a6219e9626bed696481c43bcb6b3961aef
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2009-12-14 08:08:19 +0000

    Ajout d'une colonne permettant de renseigner l'avancement de la délibération dans le parapheur

--- cake_webdelib/config/sql/patches/2.0.sql_to_2.1.sql
+++ cake_webdelib/config/sql/patches/2.0.sql_to_2.1.sql
@@ -9,11 +9,14 @@ ALTER TABLE `infosupdefs` ADD `val_initiale` VARCHAR( 255 ) NULL AFTER `type` ;
 -- Modification de la table `deliberations`
 --
 ALTER TABLE `deliberations` ADD `tdt_id` INT( 11 ) NULL AFTER `num_pref` ,
-ADD `dateAR` VARCHAR( 100 ) NULL AFTER `tdt_id` ;
+ALTER TABLE `deliberations` ADD `dateAR` VARCHAR( 100 ) NULL AFTER `tdt_id` ;
+
+ALTER TABLE `deliberations` ADD `etat_parapheur` TINYINT NULL AFTER `etat` 
 
 ALTER TABLE `deliberations` ADD `delib_pdf` LONGBLOB NULL AFTER `vote_commentaire` ;
 ALTER TABLE `seances` ADD `commentaire` VARCHAR( 500 ) NULL AFTER `traitee`;
 ALTER TABLE `commentaires` ADD `commentaire_auto` TINYINT( 1 ) NOT NULL AFTER `pris_en_compte` 
+
 
 UPDATE `aros_acos` SET `_create` = '1',
 `_read` = '1',
