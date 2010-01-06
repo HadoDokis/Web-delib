00bcbc210555c3341dc2b3ae501822fc9738c4f1
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2010-01-06 15:40:48 +0000

    Suppression de champs de validation (qui n'existe plus).

--- trunk/cake_webdelib/models/annex.php
+++ trunk/cake_webdelib/models/annex.php
@@ -5,11 +5,12 @@ class Annex extends AppModel {
 	var $displayField="titre";
 	var $validate = array(
 		'delib_id' => VALID_NOT_EMPTY,
-		'chemin' => VALID_NOT_EMPTY,
+//		'chemin' => VALID_NOT_EMPTY,
 		'titre' => VALID_NOT_EMPTY,
-		'reference_id' => VALID_NOT_EMPTY,
+//		'reference_id' => VALID_NOT_EMPTY,
 	);
 	
 	var $belongsTo = "Deliberation";
+
 }
 ?>
