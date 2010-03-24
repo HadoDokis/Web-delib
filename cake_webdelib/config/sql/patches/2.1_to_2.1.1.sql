892526f48f64e530a5d02358900c77fe1ca4cb2d
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2010-03-24 16:20:53 +0000

    Ajout de la table historiques

--- cake_webdelib/config/sql/patches/2.1_to_2.1.1.sql
+++ cake_webdelib/config/sql/patches/2.1_to_2.1.1.sql
@@ -1,2 +1,11 @@
 ALTER TABLE `deliberations` ADD `etat_asalae` TINYINT( 1 ) NULL AFTER `etat_parapheur` ;
 
+CREATE TABLE `historiques` (
+`id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`delib_id` INT( 11 ) NOT NULL ,
+`user_id` INT( 11 ) NOT NULL ,
+`circuit_id` INT( 11 ) NOT NULL ,
+`commentaire` VARCHAR( 1000 ) NOT NULL ,
+`modified` DATETIME NOT NULL ,
+`created` DATETIME NOT NULL
+) ENGINE = MYISAM ;
