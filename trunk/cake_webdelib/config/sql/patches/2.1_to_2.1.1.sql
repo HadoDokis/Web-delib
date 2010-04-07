8b1c350571f2dd08be645076b889ae222566055a
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2010-04-07 15:14:44 +0000

    Mise à jour de la structure de la table pour sauvegarder les débats saisis en commission.

--- trunk/cake_webdelib/config/sql/patches/2.1_to_2.1.1.sql
+++ trunk/cake_webdelib/config/sql/patches/2.1_to_2.1.1.sql
@@ -1,6 +1,11 @@
 ALTER TABLE `deliberations` ADD `etat_asalae` TINYINT( 1 ) NULL AFTER `etat_parapheur` ;
 ALTER TABLE `deliberations` ADD `signature` BLOB NULL AFTER `delib_pdf` ;
 
+ALTER TABLE `deliberations` ADD `commission` LONGBLOB NULL AFTER `debat_type` ,
+ADD `commission_size` INT( 11 ) NULL AFTER `commission` ,
+ADD `commission_type` VARCHAR( 255 ) NULL AFTER `commission_size` ,
+ADD `commission_name` VARCHAR( 255 ) NULL AFTER `commission_type` ;
+
 CREATE TABLE `historiques` (
 `id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 `delib_id` INT( 11 ) NOT NULL ,
