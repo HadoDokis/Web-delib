f55b0f6e50509f732a7505cf306df7dd7cb80a8c
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2010-04-12 08:53:37 +0000

    Déplacement de la requete sur les commissions

--- trunk/cake_webdelib/config/sql/patches/2.1_to_2.1.1.sql
+++ trunk/cake_webdelib/config/sql/patches/2.1_to_2.1.1.sql
@@ -1,10 +1,3 @@
-ALTER TABLE `deliberations` ADD `etat_asalae` TINYINT( 1 ) NULL AFTER `etat_parapheur` ;
-ALTER TABLE `deliberations` ADD `signature` BLOB NULL AFTER `delib_pdf` ;
-
-ALTER TABLE `deliberations` ADD `commission` LONGBLOB NULL AFTER `debat_type` ,
-ADD `commission_size` INT( 11 ) NULL AFTER `commission` ,
-ADD `commission_type` VARCHAR( 255 ) NULL AFTER `commission_size` ,
-ADD `commission_name` VARCHAR( 255 ) NULL AFTER `commission_type` ;
 
 CREATE TABLE `historiques` (
 `id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
@@ -16,3 +9,10 @@ CREATE TABLE `historiques` (
 `created` DATETIME NOT NULL
 ) ENGINE = MYISAM ;
 
+ALTER TABLE `deliberations` ADD `etat_asalae` TINYINT( 1 ) NULL AFTER `etat_parapheur` ;
+ALTER TABLE `deliberations` ADD `signature` BLOB NULL AFTER `delib_pdf` ;
+
+ALTER TABLE `deliberations` ADD `commission` LONGBLOB NULL AFTER `debat_type` ,
+ADD `commission_size` INT( 11 ) NULL AFTER `commission` ,
+ADD `commission_type` VARCHAR( 255 ) NULL AFTER `commission_size` ,
+ADD `commission_name` VARCHAR( 255 ) NULL AFTER `commission_type` ;
