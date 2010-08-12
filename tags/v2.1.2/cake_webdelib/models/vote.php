fd6dc51e20d7bbb1055683be6dcd9c7336a3df2b
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2010-08-12 15:48:10 +0000

    Etiquetage de la version 2.1.2

---
+++ tags/v2.1.2/cake_webdelib/models/vote.php
@@ -0,0 +1,16 @@
+<?php
+class Vote extends AppModel {
+
+	var $name = 'Vote';
+        var $belongsTo = array(
+                        'Acteur' =>
+                                array('className' => 'Acteur',
+                                                'foreignKey' => 'acteur_id',
+                                                'conditions' => '',
+                                                'fields' => '',
+                                                'order' => '',
+                                                'counterCache' => ''
+                                     )
+                        );
+}
+?>
