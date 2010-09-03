b105407ccadb9fc7a76d44eda3df7dba1d102986
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2010-09-03 15:06:02 +0000

    Ajout d'une fonction générant la liste des natures

--- cake_webdelib/models/nature.php
+++ cake_webdelib/models/nature.php
@@ -1,5 +1,21 @@
 <?php
     class Nature extends AppModel {
        var $name = 'Nature';
+         /* retourne la liste des natures [id]=>[libelle] pour utilisation html->selectTag */
+        function generateList($order_by=null) {
+                $generateList = array();
+
+                if ($order_by==null)
+                    $natures = $this->find('all', array('fields'=>'id, libelle'));
+                else
+                    $natures = $this->find('all', array('fields'=> 'id, libelle', 'order' => $order_by.' DESC'));
+
+                foreach($natures as $nature) {
+                        $generateList[$nature['Nature']['id']] = $nature['Nature']['libelle'];
+                }
+
+                return $generateList;
+        }
+
     }
 ?>
