b09c74eeb9deb0b4e27feacc3244c1d2c4c081e5
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2010-09-17 07:36:45 +0000

    Nom de l'étape = prenom utilisateur + nom utilisateur

--- trunk/cake_webdelib/vendors/shells/maj_circuit.php
+++ trunk/cake_webdelib/vendors/shells/maj_circuit.php
@@ -21,10 +21,15 @@
                                                  FROM users_circuits 
                                                  WHERE circuit_id = ". $circuit['circuits']['id']." 
                                                  ORDER BY position");
+
                  foreach ($users as $user){
+	                 $userInfo = $this->Circuit->query("SELECT nom, prenom, login 
+                                                 FROM users
+                                                 WHERE id = ". $user['users_circuits']['user_id']);
+
                      $this->Etape->create();
                      $etape['Etape']['circuit_id'] = $circuit_id;
-                     $etape['Etape']['nom'] = 'ETAPE_'.$user['users_circuits']['id'] ;
+                     $etape['Etape']['nom'] = $userInfo[0]['users']['prenom'].' '.$userInfo[0]['users']['nom'] ;
                      $etape['Etape']['ordre'] = $user['users_circuits']['position'];
                      $etape['Etape']['type'] = 1;
                      $this->Etape->save($etape);
@@ -32,14 +37,16 @@
                      $etape_id = $this->Etape->getLastInsertID();
                      $this->Composition->create(); 
                      $compositon['Composition']['etape_id'] =  $etape_id ;
-                     $compositon['Composition']['user_id']  = $user['users_circuits']['user_id'];
+                     $compositon['Composition']['trigger_id']  = $user['users_circuits']['user_id'];
                      $compositon['Composition']['type_validation']  = 'V';
                      $this->Composition->save($compositon); 
                  } 
             }
-            $this->Circuit->query("DROP circuits");
-            $this->Circuit->query("DROP users_circuits");
-    
+
+			$this->out('Creation des circuits de traitement effectuee');
+			$this->out('Vous pouvez supprimer les tables \'users_circuits\' et \'circuits\'.');
+//            $this->Circuit->query("DROP TABLE circuits");
+//            $this->Circuit->query("DROP TABLE users_circuits");
         }
     }
 
