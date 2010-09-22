5e9fb7614cfb6de6c54cc7cf6a4f128a44240f37
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2010-09-22 07:40:04 +0000

    ajout de la fonction listFields

--- cake_webdelib/app_model.php
+++ cake_webdelib/app_model.php
@@ -39,6 +39,50 @@
  */
 class AppModel extends Model{
 
+ /*
+ * Equivalent du find('list') mais sur plusieurs champs du model
+ * $params permet de passer tous les paramètres à la fonction sous la forme :
+ * array(
+ *		'fields' => array('field1', 'field2'), //array of field names
+ *		'format' => string '%s, %s ....', (format utilisé dans la fonction php printf)
+ *		'conditions' => array('field' => $thisValue), //array of conditions
+ *		'order' => array('Model.field1', 'Model.field2 DESC') //string or array defining order
+ *		);
+ * Les clés manquantes sont initialisées par la variable du modèle $displayFields
+ * Si 'fields' ou 'format' sont vides ou ne sont pas définis, alors la fonction retourne find('list')
+ */
+function listFields($params = array()) {
+	// Initialisation des clés manquantes de $params avec les valeurs de $this->$displayFields
+	if (isset($this->displayFields))
+		$params = array_merge($this->displayFields, $params);
+
+	// Si la liste des champs ou le format ne sont pas définis on retourne la fonction find('list')
+	if (empty($params['fields']) || empty($params['format']))
+		return $this->find('list', $params);
+
+	// Ajout de la clé primaire dans la liste des champs si elle n'y est pas déjà
+	$clePrimaireAjoutee = false;
+	if (!in_array($this->primaryKey, $params['fields'])) {
+		$params['fields'][] = $this->primaryKey;
+		$clePrimaireAjoutee = true;
+	}
+
+	// On force la récursivite à -1
+	$params['recursive'] = -1;
+
+	// Execution du find
+	$recs = $this->find('all', $params);
+
+	// Constitution de la liste de retour
+	$ret = array();
+	foreach($recs as $rec) {
+		$id = $rec[$this->alias][$this->primaryKey];
+		if ($clePrimaireAjoutee) unset($rec[$this->alias][$this->primaryKey]);
+		$ret[$id] = vsprintf($params['format'], $rec[$this->alias]);
+	}
+
+	return $ret;
+}
 
 /*function isUnique($field, $value, $id)
     {
