8a083d7c9a3e026052f0b1d20db24d2ef8c6ec01
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2010-10-15 14:53:55 +0000

    suppression de l'Etiquette de la version 3.0

--- tags/v3.0/cake_webdelib/config/sql/patches/1.0_to_1.1.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `commentaires`
-ADD `pris_en_compte` TINYINT NOT NULL DEFAULT '0' AFTER `texte` ;
-
-ALTER TABLE `seances` ADD `debat_global_name` VARCHAR( 75 ) NOT NULL ,
-ADD `debat_global_size` INT( 11 ) NOT NULL ,
-ADD `debat_global_type` VARCHAR( 255 ) NOT NULL ;
-
-INSERT INTO `models` (`id`, `modele`, `type`, `name`, `size`, `extension`, `content`) VALUES
-(15, 'Liste abstenants', 'Composant documentaire', NULL, 0, NULL, 0x235052454e4f4d5f41425354454e414e542320234e4f4d5f41425354454e414e54232d2d);
--- tags/v3.0/cake_webdelib/views/pages/mes_projets.ctp
+++
@@ -1,9 +0,0 @@
-<div id="content">
-<?php
-    $userId = $session->read('user.User.id');
-    if ($Xacl->check($userId, 'Deliberations:mesProjetsRedaction')) {
-        echo $this->requestAction('/deliberations/mesProjetsRedaction', array('return'));
-        echo('<br/>');
-    }
-?>
-</div>
