690aab179fdedb1c35926d5b05057d57ef90ebc1
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2011-06-10 09:10:02 +0000

    fonction download() : optimisation de la fonction

--- cake_webdelib/controllers/annexes_controller.php
+++ cake_webdelib/controllers/annexes_controller.php
@@ -5,47 +5,23 @@ class AnnexesController extends AppController {
 	var $aucunDroit;
 
 	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash('Invalide id pour le commentaire');
-			$this->redirect('/');
-		}
 		if ($this->Annex->del($id)) {
 		    $this->redirect($this->Session->read('user.User.lasturl'));
 		}
 	}
 
 	function download($id=null){
-		header('Content-type: '.$this->getFileType($id));
-		header('Content-Length: '.$this->getSize($id));
-		header('Content-Disposition: attachment; filename='.$this->getFileName($id));
-		echo $this->getData($id);
+		// lecture en base
+		$annexe = $this->Annex->find('first', array(
+			'recursive' => -1,
+			'conditions' => array('id'=>$id)));
+		if (empty($annexe)) return;
+		header('Content-type: '.$annexe['Annex']['filetype']);
+		header('Content-Length: '.$annexe['Annex']['size']);
+		header('Content-Disposition: attachment; filename='.$annexe['Annex']['filename']);
+		echo $annexe['Annex']['data'];
 		exit();
 	}
-
-	function getFileType($id=null) {
-		$condition = array("Annex.id" => $id);
-       	$objCourant = $this->Annex->find('all', array('conditions'=>$condition));
-		return $objCourant['0']['Annex']['filetype'];
-	}
-
-	function getFileName($id=null) {
-		$condition = array("Annex.id" => $id);
-       	$objCourant = $this->Annex->find('all', array('conditions'=>$condition));
-		return $objCourant['0']['Annex']['filename'];
-	}
-
-	function getSize($id=null) {
-		$condition = array("Annex.id" => $id);
-       	$objCourant = $this->Annex->find('all', array('conditions'=>$condition));
-		return $objCourant['0']['Annex']['size'];
-	}
-
-	function getData($id=null) {
-		$condition = array("Annex.id" => $id);
-       	$objCourant = $this->Annex->find('all', array('conditions'=>$condition));
-		return $objCourant['0']['Annex']['data'];
-	}
-
 
 }
 ?>
