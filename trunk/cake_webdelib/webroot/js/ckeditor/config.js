bda1bf0d251d811e21f8d601492b8d2180bb2023
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2011-06-21 08:45:26 +0000

    Mise a jour de la toolbar de l'éditeur en ligne

--- trunk/cake_webdelib/webroot/js/ckeditor/config.js
+++ trunk/cake_webdelib/webroot/js/ckeditor/config.js
@@ -8,4 +8,18 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+       config.toolbar = 'MyToolbar';
+ 
+	config.toolbar_MyToolbar =
+	[
+		{ name: 'document', items : [ 'Source','Save', '-', 'Preview' ] },
+		{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+		{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
+                '/',
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Strike','-','RemoveFormat' ] },
+		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','Blockquote' ] },
+		{ name: 'insert', items : [ 'Image','Table','SpecialChar'] },
+            	{ name: 'colors', items : [ 'TextColor','BGColor' ] },
+		{ name: 'tools', items : [ 'Maximize','-','About' ] }
+	];
 };
