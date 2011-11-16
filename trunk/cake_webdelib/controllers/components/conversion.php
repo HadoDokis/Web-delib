141abf4d993bb110ffea2375337bae55f3df8be5
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2011-11-16 09:27:52 +0000

    Ajout de la gestion de CLOUDOOo

--- trunk/cake_webdelib/controllers/components/conversion.php
+++ trunk/cake_webdelib/controllers/components/conversion.php
@@ -21,15 +21,15 @@ function convertirFichier($fileName, $format) {
 	}
 
 	switch($convertorType) {
-		case 'UNOCONV' :
-			// lecture fichier exécutable de unoconv
-			$convertorExec = Configure::read('CONVERSION_EXEC');
-			if (empty($convertorExec)) {
-				return false;
-			}
-			// exécution
-			$cmd = "$convertorExec --stdout -f $format $fileName";
-			$result = shell_exec($cmd);
+	    case 'UNOCONV' :
+		// lecture fichier exécutable de unoconv
+		$convertorExec = Configure::read('CONVERSION_EXEC');
+		if (empty($convertorExec)) {
+		    return false;
+		}
+		// exécution
+		$cmd = "$convertorExec --stdout -f $format $fileName";
+		$result = shell_exec($cmd);
 	        // guess that if there is less than this characters probably an error
 	        if (strlen($result) < 10) {
                     return false;
@@ -37,8 +37,30 @@ function convertirFichier($fileName, $format) {
                     return ($result);
 	        }
 		break;
+	    case 'CLOUDOOO' : 
+                require_once 'XML/RPC.php';
+		$content =  base64_encode(file_get_contents($fileName));
+		$fileinfo =  pathinfo($fileName);
+                if ($fileinfo['extension'] == 'pdf') $fileinfo['extension'] = 'odt';
+ 
+                $params = array( new XML_RPC_Value($content, 'string'),
+                                 new XML_RPC_Value($fileinfo['extension'],    'string'),
+                                 new XML_RPC_Value($format,    'string'),
+                                 new XML_RPC_Value(false,      'boolean'),
+				 new XML_RPC_Value(true,       'boolean'));
+
+                $url = Configure::read('CLOUDOOO_HOST').":".Configure::read('CLOUDOOO_PORT');
+               
+                $msg = new XML_RPC_Message('convertFile', $params);
+                $cli = new XML_RPC_Client('/', $url);
+                $resp = $cli->send($msg);
+                return (base64_decode($resp->xv->me['string']));
+           	break;       
 	}
         return false;
 }
+
+
+
 
 }?>
