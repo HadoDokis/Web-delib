079e0a110beef2f07adca29c854fb76f71e71e57
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2011-11-16 10:28:08 +0000

    Mise a jour des scripts de vérification des pré-requis

--- cake_webdelib/webroot/check/header.php
+++ cake_webdelib/webroot/check/header.php
@@ -1,9 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-<head>
-    <title>CHECK</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15" />
-    <link rel="stylesheet" type="text/css" href="check.css" />
-</head>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US"> 
+<head> 
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+	<script type="text/javascript" src="../js/jquery-1.6.2.js"></script>
+	<script type="text/javascript" src="scripts/expand.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    // --- first section initially expanded:
+		    $("h2.expand").toggler({initShow: "div.collapse:first"});
+		    $("#content").expandAll({trigger: "h2.expand", ref: "div.demo",  speed: 300, oneSwitch: false});
+		});
+	</script>
+	<!--<![endif]-->
+	<title>as@lae check</title>
+	<link rel="stylesheet" type="text/css" href="css/check.css" />
+	<link rel="stylesheet" type="text/css" href="css/effect.css" />
+	<div id="headerLogoContainer">
+		<img alt="" src="../img/logoAsalae60.png">
+	</div>
+</head> 
 <body>
