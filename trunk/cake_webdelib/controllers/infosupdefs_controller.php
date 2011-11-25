00ea7a6ad000f165bfc159c2f4b5d94a9f5e14b1
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2011-11-25 09:10:37 +0000

    modifications pour prendre en compte les modeles 'Deliberation' et 'Seance'

--- trunk/cake_webdelib/controllers/infosupdefs_controller.php
+++ trunk/cake_webdelib/controllers/infosupdefs_controller.php
@@ -20,35 +20,46 @@ class InfosupdefsController extends AppController
 	}
 
 	function index() {
-		$this->data = $this->Infosupdef->find('all',  array('conditions' => array('model' => 'Deliberation'),
-								     'recursive'  => -1 ,
-                                                                     'order'      => 'ordre'));
+		$this->data = $this->Infosupdef->find('all', array(
+			'recursive' => -1 ,
+			'conditions' => array('model' => 'Deliberation'),
+			'order' => 'ordre'));
+		$this->set('titre', 'Liste des informations suppl&eacute;mentaires des projets');
+		$this->set('lienAdd', '/infosupdefs/add/Deliberation');
 	}
 
-        function index_seance() {
-	    $this->data = $this->Infosupdef->find('all', array('conditions'=>array('model' => 'Seance'),
-                                                                'recursive'  => -1 ,
-                                                                'order'      => 'ordre'));
-        }
+	function index_seance() {
+		$this->data = $this->Infosupdef->find('all', array(
+			'recursive' => -1 ,
+			'conditions' => array('model' => 'Seance'),
+			'order' => 'ordre'));
+		$this->set('titre', 'Liste des informations suppl&eacute;mentaires des s&eacute;ances');
+		$this->set('lienAdd', '/infosupdefs/add/Seance');
+		$this->render('index');
+	}
 
 
 	function view($id = null) {
 		$this->data = $this->{$this->modelClass}->findById($id, null, null, -1);
 		if (empty($this->data)) {
-			$this->Session->setFlash('Invalide id pour l\'information suppl&eacute;mentaire : &eacute;dition impossible');
+			$this->Session->setFlash('Invalide id pour l\'information suppl&eacute;mentaire : &eacute;dition impossible', 'growl');
 			$this->redirect('/infosupdefs/index');
 		} else {
 			$this->data['Infosupdef']['libelleType'] = $this->Infosupdef->libelleType($this->data['Infosupdef']['type']);
 			$this->data['Infosupdef']['libelleRecherche'] = $this->Infosupdef->libelleRecherche($this->data['Infosupdef']['recherche']);
+			$this->set('titre', 'Fiche information suppl&eacute;mentaire de '.($this->data['Infosupdef']['model']=='Deliberation'?'d&eacute;lib&eacute;ration':'s&eacute;ance'));
+			$this->set('lienRetour', '/infosupdefs/'.($this->data['Infosupdef']['model']=='Deliberation'?'index':'index_seance'));
 		}
 	}
 
-	function add($model) {
+	function add($model=null) {
+		// initialisations
 		$sortie = false;
-                $this->set('model', $model);
-		if (!empty($this->data)) {
+		$codePropose = '';
+		
+		if (empty($this->data)) {
 			$this->data['Infosupdef']['model'] = $model;
-
+		} else {
 			/* traitement de la valeur par defaut */
 			if ($this->data['Infosupdef']['type'] == 'date')
 				$this->data['Infosupdef']['val_initiale'] = $this->data['Infosupdef']['val_initiale_date'];
@@ -58,31 +69,36 @@ class InfosupdefsController extends AppController
 				$this->data['Infosupdef']['val_initiale'] = '';
 
 			if ($this->{$this->modelClass}->save($this->data)) {
-				$this->Session->setFlash('L\'information suppl&eacute;mentaire \''.$this->data['Infosupdef']['nom'].'\' a &eacute;t&eacute; ajout&eacute;e');
+				$this->Session->setFlash('L\'information suppl&eacute;mentaire \''.$this->data['Infosupdef']['nom'].'\' a &eacute;t&eacute; ajout&eacute;e', 'growl');
 				$sortie = true;
-			} else
-				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.');
+			} else {
+				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.', 'growl');
+				$codePropose = Inflector::variable($this->data['Infosupdef']['code']);
+			}
 		}
+		$lienRetour = '/infosupdefs/'.($this->data['Infosupdef']['model']=='Deliberation'?'index':'index_seance');
 		if ($sortie)
-			$this->redirect('/infosupdefs/index');
+			$this->redirect($lienRetour);
 		else {
 			$this->set('types', $this->{$this->modelClass}->generateListType());
 			$this->set('listEditBoolean', $this->{$this->modelClass}->listEditBoolean);
-			if (isset($this->data['Infosupdef']))
-				$this->set('codePropose', Inflector::variable($this->data['Infosupdef']['code']));
-			else
-				$this->set('codePropose', '');
+			$this->set('codePropose', $codePropose);
+			$this->set('titre', 'Ajout d\'une information suppl&eacute;mentaire de '.($this->data['Infosupdef']['model']=='Deliberation'?'d&eacute;lib&eacute;ration':'s&eacute;ance'));
+			$this->set('lienRetour', $lienRetour);
 
 			$this->render('edit');
 		}
 	}
 
 	function edit($id = null) {
+		// initialisations
 		$sortie = false;
+		$codePropose = '';
+
 		if (empty($this->data)) {
 			$this->data = $this->{$this->modelClass}->findById($id, null, null, -1);
 			if (empty($this->data)) {
-				$this->Session->setFlash('Invalide id pour l\'information suppl&eacute;mentaire : &eacute;dition impossible');
+				$this->Session->setFlash('Invalide id pour l\'information suppl&eacute;mentaire : &eacute;dition impossible', 'growl');
 				$sortie = true;
 			}
 			/* traitement de la valeur par defaut pour les dates et les booleens */
@@ -91,7 +107,7 @@ class InfosupdefsController extends AppController
 			elseif ($this->data['Infosupdef']['type'] == 'boolean')
 				$this->data['Infosupdef']['val_initiale_boolean'] = $this->data['Infosupdef']['val_initiale'];
 		} else {
-			/* traitement de la valeur par defaut */
+			// traitement de la valeur par defaut
 			if ($this->data['Infosupdef']['type'] == 'date')
 				$this->data['Infosupdef']['val_initiale'] = $this->data['Infosupdef']['val_initiale_date'];
 			elseif ($this->data['Infosupdef']['type'] == 'boolean')
@@ -100,17 +116,22 @@ class InfosupdefsController extends AppController
 				$this->data['Infosupdef']['val_initiale'] = '';
 
 			if ($this->{$this->modelClass}->save($this->data)) {
-				$this->Session->setFlash('L\'information suppl&eacute;mentaire \''.$this->data['Infosupdef']['nom'].'\' a &eacute;t&eacute; modifi&eacute;e');
+				$this->Session->setFlash('L\'information suppl&eacute;mentaire \''.$this->data['Infosupdef']['nom'].'\' a &eacute;t&eacute; modifi&eacute;e', 'growl');
 				$sortie = true;
-			} else
-				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.');
+			} else {
+				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.', 'growl');
+				$codePropose = Inflector::variable($this->data['Infosupdef']['code']);
+			}
 		}
+		$lienRetour = '/infosupdefs/'.($this->data['Infosupdef']['model']=='Deliberation'?'index':'index_seance');
 		if ($sortie)
-			$this->redirect('/infosupdefs/index');
+			$this->redirect($lienRetour);
 		else {
 			$this->set('types', $this->{$this->modelClass}->generateListType());
 			$this->set('listEditBoolean', $this->{$this->modelClass}->listEditBoolean);
-			$this->set('codePropose', Inflector::variable($this->data['Infosupdef']['code']));
+			$this->set('codePropose', $codePropose);
+			$this->set('titre', 'Edition d\'une information suppl&eacute;mentaire de '.($this->data['Infosupdef']['model']=='Deliberation'?'d&eacute;lib&eacute;ration':'s&eacute;ance'));
+			$this->set('lienRetour', $lienRetour);
 		}
 	}
 
@@ -118,26 +139,28 @@ class InfosupdefsController extends AppController
 		$messageErreur = '';
 		$aSupprimer = $this->{$this->modelClass}->findById($id, null, null, -1);
 		if (empty($aSupprimer))
-			$this->Session->setFlash('Invalide id pour l\'information suppl&eacute;mentaire : suppression impossible');
+			$this->Session->setFlash('Invalide id pour l\'information suppl&eacute;mentaire : suppression impossible', 'growl');
 		elseif (!$this->{$this->modelClass}->isDeletable($aSupprimer, $messageErreur))
-			$this->Session->setFlash($messageErreur);
+			$this->Session->setFlash($messageErreur, 'growl');
 		elseif ($this->{$this->modelClass}->delete($id)) {
 			$this->{$this->modelClass}->Infosuplistedef->delList($id);
-			$this->Session->setFlash('L\'information suppl&eacute;mentaire \''.$aSupprimer['Infosupdef']['nom'].'\' a &eacute;t&eacute; supprim&eacute;e');
+			$this->Session->setFlash('L\'information suppl&eacute;mentaire \''.$aSupprimer['Infosupdef']['nom'].'\' a &eacute;t&eacute; supprim&eacute;e', 'growl');
 		}
 
-		$this->redirect('/infosupdefs/index');
+		$this->redirect($this->referer());
 	}
 
-	function changerOrdre($id = null, $suivant = true)
-	{
-		$this->data = $this->{$this->modelClass}->findById($id, null, null, -1);
-		if (empty($this->data))
-			$this->Session->setFlash('Invalide id : deplacement impossible.');
+	function changerOrdre($id = null, $suivant = true) {
+		$infoSup = $this->{$this->modelClass}->find('first', array(
+			'recursive'=>-1,
+			'fields' => array('id', 'model'),
+			'conditions' => array('id'=>$id)));
+		if (empty($infoSup))
+			$this->Session->setFlash('Invalide id : deplacement impossible.', 'growl');
 		else
 			$this->{$this->modelClass}->invert($id, $suivant);
 
-		$this->redirect('/infosupdefs/index');
+		$this->redirect($this->referer());
 	}
 
 }
