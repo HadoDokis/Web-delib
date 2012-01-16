80ca0e8c971d0ddcd521dce0b3e1215cc3474e46
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2012-01-16 13:51:25 +0000

    Ajout d'un composant permettant des manipulations sur des pdf ( pour l'instant, que la concaténation)

---
+++ trunk/cake_webdelib/controllers/components/pdf.php
@@ -0,0 +1,24 @@
+<?php
+
+    class PdfComponent extends Object {
+
+	function concatener($document_path, $annexes=array()) {
+	    $output = array();
+            if (empty( $annexes))
+                return true;
+            $pdftk_exec = Configure::read('PDFTK_EXEC');
+	    if (!file_exists( $pdftk_exec)){
+                $this->log("L'executable pdftk n'existe pas");
+                return false;
+	    }
+	    $annexes_path = implode(" ", $annexes);
+            $doc_orig = $document_path.'-orig';
+            rename($document_path, $doc_orig);
+            $commande = "$pdftk_exec $doc_orig  $annexes_path output $document_path";
+            $this->log($commande);
+	    exec($commande, $output, $return_value);
+              
+            return ($return_value ==0);
+        }  
+    }
+?>
