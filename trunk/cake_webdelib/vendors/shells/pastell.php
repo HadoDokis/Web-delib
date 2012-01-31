6a9322458c333edb510850ae3a697fee9b61686d
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2012-01-31 09:54:09 +0000

    Envoi des documents à PASTELL.

--- trunk/cake_webdelib/vendors/shells/pastell.php
+++ trunk/cake_webdelib/vendors/shells/pastell.php
@@ -14,19 +14,30 @@
             $id_e =  $collectivite['Collectivite']['id_entity'];
             // Controle de l'avancement des délibérations dans le parapheur
             $delibs = $this->Deliberation->find('all',
-						array('conditions' => array('Deliberation.pastell_id !=' => null )));
+						array('conditions' => array('Deliberation.pastell_id !=' => null ),
+                                                      'recursive'  => -1, 
+                                                      'fields'     =>array('id', 'pastell_id')));
 	    if (!empty($delibs)){
                  App::import('Component','Pastell');
                  $this->Pastell = new PastellComponent();
 
-//		foreach ($delibs as $delib) {
+		foreach ($delibs as $delib) {
+                    $this->log($delib);
                     $delib_id = $delib['Deliberation']['id'];
-		    //$id_d     = $delib['Deliberation']['pastell_id'];
-                    $id_d     = "NOhEp91";
-		    echo ("$delib_id -> $id_d\n");
+		    $id_d     = $delib['Deliberation']['pastell_id'];
 		    $infos = $this->Pastell->getInfosDocument($id_e, $id_d);
-                    debug($infos);
-//              }
+                    $infos = (array)$infos;
+                    if (isset($infos['action-possible'])) {
+                        foreach($infos['action-possible'] as $id=>$action) {
+                            if ($action == 'verif-iparapheur') {
+                                $this->Pastell->action($id_e, $id_d, $action);
+		                $infos = $this->Pastell->getInfosDocument($id_e, $id_d);
+                                $infos = (array)$infos;
+                                $this->log($infos);
+                            }
+                        }
+                    }
+                }
             }
         }
         
