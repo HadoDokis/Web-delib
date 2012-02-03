2bd23e18bcabee7b7bbed884855dcb1c76a6b4fa
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2012-02-03 12:52:24 +0000

    Avancé dans le développement Webdelib/Pastell

--- cake_webdelib/vendors/shells/pastell.php
+++ cake_webdelib/vendors/shells/pastell.php
@@ -14,7 +14,7 @@
             $id_e =  $collectivite['Collectivite']['id_entity'];
             // Controle de l'avancement des délibérations dans le parapheur
             $delibs = $this->Deliberation->find('all',
-						array('conditions' => array('Deliberation.pastell_id !=' => null ),
+						array('conditions' => array('Deliberation.pastell_id !=' => null),
                                                       'recursive'  => -1, 
                                                       'fields'     =>array('id', 'pastell_id')));
 	    if (!empty($delibs)){
@@ -22,7 +22,6 @@
                  $this->Pastell = new PastellComponent();
 
 		foreach ($delibs as $delib) {
-                    $this->log($delib);
                     $delib_id = $delib['Deliberation']['id'];
 		    $id_d     = $delib['Deliberation']['pastell_id'];
 		    $infos = $this->Pastell->getInfosDocument($id_e, $id_d);
@@ -30,18 +29,26 @@
                     if (isset($infos['action-possible'])) {
                         foreach($infos['action-possible'] as $id=>$action) {
                             if ($action == 'verif-iparapheur') {
-                                $this->Pastell->action($id_e, $id_d, $action);
-		                $infos = $this->Pastell->getInfosDocument($id_e, $id_d);
-                                $infos = (array)$infos;
-                                $this->log($infos);
+                                $return = $this->Pastell->action($id_e, $id_d, $action);
+                                $return = (array) $return;
+                                if ($return['result']) 
+                                    $this->stockSignature($id_e, $id_d, $delib_id);
                             }
                         }
                     }
                 }
             }
         }
-        
 
-        
+        function stockSignature ($id_e, $id_d, $delib_id) {
+            App::import('Component','Pastell');
+            $this->Pastell = new PastellComponent();
+
+            $this->Deliberation->id = $delib_id;
+            $signature =  $this->Pastell->getFile($id_e, $id_d, "signature&num=0");
+            $this->Deliberation->savefield("signee", 1); 
+            $this->Deliberation->savefield("signature", $signature); 
+            return true; 
+        }
     }
 ?>
