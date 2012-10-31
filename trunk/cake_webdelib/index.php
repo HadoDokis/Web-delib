6804386852b5b7c18eeba81e8789f1be67985591
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2012-10-31 15:16:28 +0000

    Portage en CakePHP 2.2.x

--- trunk/cake_webdelib/index.php
+++ trunk/cake_webdelib/index.php
@@ -1,26 +1,17 @@
 <?php
-/* SVN FILE: $Id: index.php 4409 2007-02-02 13:20:59Z phpnut $ */
 /**
- * PHP versions 4 and 5
+ * PHP 5
  *
- * CakePHP(tm) :  Rapid Development Framework <http://www.cakephp.org/>
- * Copyright 2005-2007, Cake Software Foundation, Inc.
- *								1785 E. Sahara Avenue, Suite 490-204
- *								Las Vegas, Nevada 89104
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @filesource
- * @copyright		Copyright 2005-2007, Cake Software Foundation, Inc.
- * @link				http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
- * @package			cake
- * @subpackage		cake.app
- * @since			CakePHP(tm) v 0.10.0.1076
- * @version			$Revision: 4409 $
- * @modifiedby		$LastChangedBy: phpnut $
- * @lastmodified	$Date: 2007-02-02 07:20:59 -0600 (Fri, 02 Feb 2007) $
- * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app
+ * @since         CakePHP(tm) v 0.10.0.1076
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
-?>
