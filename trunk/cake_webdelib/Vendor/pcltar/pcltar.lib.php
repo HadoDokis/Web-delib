f60616ade0459fee903eb1f7fa78a5dc1230d748
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2012-11-21 15:38:22 +0000

    Portage CakePHP 2.2.x

--- trunk/cake_webdelib/Vendor/pcltar/pcltar.lib.php
+++ trunk/cake_webdelib/Vendor/pcltar/pcltar.lib.php
@@ -65,11 +65,11 @@ if (!defined("PCL_TAR"))
   // Library in order to limit the potential 'lib' directory path problem.
   if (!defined("PCLERROR_LIB"))
   {
-    include(APP.'vendors/pcltar/pclerror.lib.php');
+    include(ROOT.DS.APP_DIR.DS.'Vendor/pcltar/pclerror.lib.php');
   }
   if (!defined("PCLTRACE_LIB"))
   {
-    include(APP.'vendors/pcltar/pcltrace.lib.php');
+    include(ROOT.DS.APP_DIR.DS.'Vendor/pcltar/pcltrace.lib.php');
   }
 
   // --------------------------------------------------------------------------------
