e5bbd6e6d97dad054a3e13199370d69449be7f8b
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2012-11-22 13:35:54 +0000

    Mise à jour de la version de Webdelib

--- cake_webdelib/Config/core.php
+++ cake_webdelib/Config/core.php
@@ -32,7 +32,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 1);
+	Configure::write('debug', 0);
 /**
  * Configure the Error handler used to handle errors for your application.  By default
  * ErrorHandler::handleError() is used.  It will display errors using Debugger, when debug > 0
@@ -293,6 +293,6 @@ Cache::config('_cake_model_', array(
     define('CONFIG_PATH', $path2."Config/");
     define ('CRON_DISPATCHER', false);
     require_once ('webdelib.inc');
-    define('VERSION', '<b> 4.0-rc1 </b>');
+    define('VERSION', '<b> 4.0 </b>');
     include_once(ROOT.DS.APP_DIR.DS.'Plugin'.DS.'Cakeflow'.DS.'Config'.DS.'cakeflow.conf.php');
 
