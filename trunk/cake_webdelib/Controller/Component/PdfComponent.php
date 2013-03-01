10120c683b9bc864d091c01a5c16b32c57cfae73
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-03-01 10:33:29 +0000

    Ajout de la fonction pour le découpage des PDFs en différentes images.

--- trunk/cake_webdelib/Controller/Component/PdfComponent.php
+++ trunk/cake_webdelib/Controller/Component/PdfComponent.php
@@ -18,5 +18,38 @@
               
             return ($return_value ==0);
         }  
+
+        function toOdt($filename){
+            $outputDir =  tempdir();
+            $GS_EXEC = Configure::read('GS_EXEC');
+            $GS_OPTS = ' -dNOPAUSE -dBATCH -sDEVICE=pngalpha -r150 -sPAPERSIZE=a4 -sOutputFile="'.$outputDir.'/%d.png"';
+
+            $output = array();
+            require_once(ROOT.DS.APP_DIR.DS.'Vendor'.DS.'odtphp'.DS.'odf.php');
+            $doc = new Odf(ROOT.DS.APP_DIR.DS.'Vendor'.DS.'odtphp'.DS.'modele.odt');
+
+            $commande = "$GS_EXEC  $GS_OPTS $filename" ;
+            exec($commande, $output, $return_value);
+            $nbPages = count(scandir($outputDir)) -1;
+            $article = $doc->setSegment('articles');
+            for($i=1; $i<$nbPages; $i++) {
+                if (file_exists($outputDir."/$i.png")){
+                    $infos = getimagesize ($outputDir."/$i.png");
+                    if (intval($infos[0]) == 1754) {
+                        $source = imagecreatefrompng($outputDir."/$i.png");
+                        $rotate = imagerotate($source, 90, 0);
+                        imagepng( $rotate, $outputDir."/$i.png");
+                        imagedestroy($source);
+                        imagedestroy($rotate);
+                    }
+                    $article->setImage('image', $outputDir."/$i.png", $i -1);
+                    
+                    $article->merge();
+                }
+            }
+            $doc->mergeSegment($article);
+            $doc->saveToDisk($outputDir."/result.odt");
+            return file_get_contents($outputDir."/result.odt");
+        }
     }
 ?>
