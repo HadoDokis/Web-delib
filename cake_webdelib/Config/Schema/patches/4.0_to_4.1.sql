1a10d69103906253c9b4dc99649afe06b63a8381
francois <francois@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-03-04 09:55:37 +0000

    AJout du patch pour la nouvelle version de webdelib.

---
+++ cake_webdelib/Config/Schema/patches/4.0_to_4.1.sql
@@ -0,0 +1,28 @@
+ALTER TABLE users ADD COLUMN zone_1 character varying(50);
+ALTER TABLE users ADD COLUMN zone_2 character varying(50);
+ALTER TABLE users ADD COLUMN zone_3 character varying(50);
+ALTER TABLE users ADD COLUMN zone_4 character varying(50);
+ALTER TABLE users ADD COLUMN zone_5 character varying(50);
+ALTER TABLE users ADD COLUMN zone_6 character varying(50);
+ALTER TABLE users ADD COLUMN zone_7 character varying(50);
+ALTER TABLE users ADD COLUMN zone_8 character varying(50);
+ALTER TABLE users ADD COLUMN zone_9 character varying(50);
+
+CREATE TABLE infosupdefs_profils (
+    id integer NOT NULL,
+    profil_id integer NOT NULL,
+    infosupdef_id integer NOT NULL
+);
+ALTER TABLE public.infosupdefs_profils OWNER TO webdelib;
+CREATE SEQUENCE infosupdefs_profils_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+ALTER TABLE public.infosupdefs_profils_id_seq OWNER TO webdelib;
+ALTER SEQUENCE infosupdefs_profils_id_seq OWNED BY infosupdefs_profils.id;
+ALTER TABLE infosupdefs_profils ALTER COLUMN id  SET DEFAULT NEXTVAL('infosupdefs_profils_id_seq');
+INSERT INTO "infosupdefs_profils" (profil_id, infosupdef_id) select profils.id, infosupdefs.id from profils, infosupdefs;
+
+
