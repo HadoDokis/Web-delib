0c1fd38a3ab51a982c4eb7e18ea68a97ef57b6b5
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-04-04 15:26:40 +0000

    nombreux bugfix délégation, correction pour compatibilité cron (requestAction -> curl), suppression des etapes de délégation au profit des composition de type délégation, nouveaux boutons, etc.

--- trunk/cake_webdelib/Console/Command/CronShell.php
+++ trunk/cake_webdelib/Console/Command/CronShell.php
@@ -1,24 +1,33 @@
 <?php
 
-//App::import('Core', 'Router');
-
 class CronShell extends Shell {
 
-    var $uses = array('Traitement');
     function startup() {
         
     }
 
     function main() {
+//        /*Gestion des droits*/
+//        App::import('Component','Auth'); 
+//        $this->Auth = new AuthComponent(null); 
+//        App::import('Component','Session'); 
+//        $this->Auth->Session = new SessionComponent(null); 
 
+        //////////////////////////////////////////////////////////////////////////////
+        // A MODIFIER SI webdelib n'est pas joignable en localhost (valeur par défaut)
+        //////////////////////////////////////////////////////////////////////////////
+        $baseUrl = FULL_BASE_URL;
+        /////////////////////////
+        $url = $baseUrl."/crons/runCrons";
+        
         // initialisation de la ressource curl
         $c = curl_init();
         // indique à curl quelle url on souhaite télécharger
-        curl_setopt($c, CURLOPT_URL, FULL_BASE_URL . "/crons/runCrons");
+        curl_setopt($c, CURLOPT_URL, $url);
         // indique à curl de ne pas retourner les headers http de la réponse dans la chaine de retour
         curl_setopt($c, CURLOPT_HEADER, false);
         // Display communication with server
-        curl_setopt($c, CURLOPT_VERBOSE, false); 
+        curl_setopt($c, CURLOPT_VERBOSE, false);
         // indique à curl de nous retourner le contenu de la requête plutôt que de l'afficher
         curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
         // execution de la requete
