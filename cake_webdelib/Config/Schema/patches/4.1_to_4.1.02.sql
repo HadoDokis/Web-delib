6fb6fe26cdff289b410b2abef4a88890dec6c98e
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-06-14 15:05:50 +0000

    ajout de la colonne id_parapheur dans la table deliberations

--- cake_webdelib/Config/Schema/patches/4.1_to_4.1.02.sql
+++ cake_webdelib/Config/Schema/patches/4.1_to_4.1.02.sql
@@ -3,9 +3,9 @@
 --
 BEGIN;
 
-
 ALTER TABLE deliberations_seances ADD COLUMN commentaire character varying(1000) NULL;
 
+ALTER TABLE deliberations ADD COLUMN id_parapheur varchar(32);
 
 --
 -- Fin du patch : fin de la transaction
