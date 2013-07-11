fe60863c81f325c2341fe6f6857855ec17e23867
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-07-11 12:43:14 +0000

    Patch 02-Beta5 de la version 4.1

---
+++ tags/v4.1.02-beta5/Controller/CommentairesController.php
@@ -0,0 +1,83 @@
+<?php
+class CommentairesController extends AppController {
+
+	// Gestion des droits
+	var $aucunDroit;
+
+	function add($delib_id=null) {
+		if (!$delib_id) {
+			$this->Session->setFlash('Invalide id pour la d&eacute;lib&eacute;ration du commentaire.');
+			$this->redirect('/');
+		}
+		$this->set('delib_id',$delib_id);
+		if (!empty($this->data)) {
+		       $this->Commentaire->create();
+			$this->request->data['Commentaire']['agent_id'] = $this->Session->read('user.User.id');
+			$this->request->data['Commentaire']['commentaire_auto'] = 0;
+			if ($this->Commentaire->save($this->data)) {
+				$this->redirect('/deliberations/traiter/'.$this->data['Commentaire']['delib_id']);
+			} else {
+				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.');
+			}
+		}
+	}
+
+	function edit($id = null,$delib_id=null) {
+		if (empty($this->data)) {
+			if (!$id) {
+				$this->Session->setFlash('Invalide id pour le commentaire');
+				$this->redirect('/deliberations/traiter/'.$delib_id);
+			}
+			$this->set('delib_id',$delib_id);
+			$this->data = $this->Commentaire->read(null, $id);
+			} else {
+				$this->data['Commentaire']['agent_id'] = $this->Session->read('user.User.id');
+			if ($this->Commentaire->save($this->data)) {
+				$this->redirect('/deliberations/traiter/'.$this->data['Commentaire']['delib_id']);
+			} else {
+				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.');
+			}
+		}
+	}
+
+	function delete($id = null,$delib_id) {
+		if (!$id) {
+			$this->Session->setFlash('Invalide id pour le commentaire');
+			$this->redirect('/deliberations/traiter/'.$delib_id);
+		}
+		if ($this->Commentaire->delete($id)) {
+			$this->redirect('/deliberations/traiter/'.$delib_id);
+		}
+	}
+
+	function view($id = null,$delib_id=null) {
+		if (!$id) {
+			$this->Session->setFlash('Invalide id pour le commentaire.');
+			$this->redirect('/deliberations/traiter'.$delib_id);
+		}
+		$this->set('commentaire', $this->Commentaire->read(null, $id));
+	}
+
+	function prendreEnCompte($id = null, $delib_id) {
+		if (!$id) {
+			$this->Session->setFlash('Invalide id pour le commentaire');
+			$this->redirect('/deliberations/traiter/'.$delib_id);
+		}
+		$this->request->data = $this->Commentaire->read(null, $id);
+		$this->request->data['Commentaire']['pris_en_compte'] = 1;
+		if ($this->Commentaire->save($this->data)) {
+			$this->redirect('/deliberations/traiter/'.$delib_id);
+		}
+	}
+        
+        function RetourTraiter($delib_id=null) {
+            $this->Session->setFlash('Invalide id pour le commentaire.');
+            if (!$delib_id) {
+                    $this->redirect('/');
+            }else {
+                    $this->redirect('/deliberations/traiter/'.$this->request->data['']);
+            }
+        }
+
+}
+?>
---
+++ tags/v4.1.02-beta5/View/Models/generer.ctp
@@ -0,0 +1,16 @@
+<h2> Liste des fichiers générés </h2>
+<script>
+    document.getElementById("pourcentage").style.display='none';
+    document.getElementById("progrbar").style.display='none';
+    document.getElementById("affiche").style.display='none';
+    document.getElementById("contTemp").style.display='none';
+</script>
+<?php
+    foreach ($listFiles as $path=> $name) {
+        if ($name != 'Documents.zip') 
+	    echo $this->Html->link($name, $path.".$format")."<br>";
+        else
+	    echo $this->Html->link($name, $path)."<br>";
+    }
+    echo ("<br /><br /><a href='/seances/listerFuturesSeances'> Retour &agrave; la liste des séances </a>");
+?>
