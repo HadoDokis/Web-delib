e7de498b4f21b8e108e5e2ac9e91f623822d5247
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-08-23 07:55:44 +0000

    formatage indentation et commentaires makeBalise

--- branches/4.2/cake_webdelib/Model/Theme.php
+++ branches/4.2/cake_webdelib/Model/Theme.php
@@ -1,73 +1,75 @@
 <?php
+
 class Theme extends AppModel {
 
-	var $name = 'Theme';
-	
-	var $displayField = "libelle";
-	
-	var $actsAs = array('Tree');
-	
-	var $validate = array(
-		'libelle' => array(
-			array(
-				'rule' => 'notEmpty',
-				'message' => 'Entrer le libellé.'
-			)
-		)
-	);
+    var $name = 'Theme';
+    var $displayField = "libelle";
+    var $actsAs = array('Tree');
+    var $validate = array(
+        'libelle' => array(
+            array(
+                'rule' => 'notEmpty',
+                'message' => 'Entrer le libellé.'
+            )
+        )
+    );
 
-    function getLevel($theme_id, $level=1) { 
+    function getLevel($theme_id, $level = 1) {
         $theme = $this->find('first', array('conditions' => array('Theme.id' => $theme_id),
-                                            'recursive'  => -1,
-                                            'fields'     => array('id', 'parent_id')));
+            'recursive' => -1,
+            'fields' => array('id', 'parent_id')));
 
-        if($theme['Theme']['parent_id']==0){
+        if ($theme['Theme']['parent_id'] == 0) {
             return ($level);
-        }
-        else {
-            $level = $level +1;
+        } else {
+            $level = $level + 1;
             return ($this->getLevel($theme['Theme']['parent_id'], $level));
         }
     }
 
-    function getLibelleParent($theme_id, $level =1, $tableau = array() ) {
+    function getLibelleParent($theme_id, $level = 1, $tableau = array()) {
         $theme = $this->find('first', array('conditions' => array('Theme.id' => $theme_id),
-                                            'recursive'  => -1,
-                                            'fields'     => array('id', 'parent_id', 'libelle')));
+            'recursive' => -1,
+            'fields' => array('id', 'parent_id', 'libelle')));
         $tableau[$level] = $theme['Theme']['libelle'];
-        if($theme['Theme']['parent_id']==0){
+        if ($theme['Theme']['parent_id'] == 0) {
             return ($tableau);
+        } else {
+            $level = $level + 1;
+            return ($this->getLibelleParent($theme['Theme']['parent_id'], $level, $tableau));
         }
-        else {
-            $level = $level +1;
-            return ($this->getLibelleParent($theme['Theme']['parent_id'], $level,  $tableau));
-        }
-
-
     }
 
-    function makeBalise(&$oMainPart, $theme_id) { 
+    /**
+     * Données Gedooo :
+     *  - theme_projet/theme.libelle/text
+     *  - critere_trie_theme/theme.order/text
+     * @param GDO_PartType &$oMainPart adresse de l'objet GDO_PartType à remplir
+     * @param integer $theme_id, identifiant du theme en base
+     */
+    function makeBalise(&$oMainPart, $theme_id) {
         $tab = array();
         $theme = $this->find('first', array('conditions' => array('Theme.id' => $theme_id),
-                                            'recursive'  => -1,
-                                            'fields'     => array('libelle', 'order', 'parent_id')));
+            'recursive' => -1,
+            'fields' => array('libelle', 'order', 'parent_id')));
 
         $arbre = $this->getLibelleParent($theme_id);
-       // $level = $this->getLevel($theme_id);
+        // $level = $this->getLevel($theme_id);
         $level = count($arbre);
-        for ($i=$level+1; $i <= 10; $i++) {
-            $oMainPart->addElement(new GDO_FieldType("T".$i."_theme", '', 'text'));
-        } 
-        foreach ($arbre as $index => $libelle) {
+        for ($i = $level + 1; $i <= 10; $i++) {
+            $oMainPart->addElement(new GDO_FieldType("T" . $i . "_theme", '', 'text'));
+        }
+        foreach ($arbre as $libelle) {
             $tab[$level] = $libelle;
-            $level--; 
+            $level--;
         }
-        for ($i =1; $i <= count($tab); $i++) {       
-            $oMainPart->addElement(new GDO_FieldType("T".$i."_theme", $tab[$i], 'text'));
+        for ($i = 1; $i <= count($tab); $i++) {
+            $oMainPart->addElement(new GDO_FieldType("T" . $i . "_theme", $tab[$i], 'text'));
         }
-        $oMainPart->addElement(new GDO_FieldType('theme_projet', $theme['Theme']['libelle'],  'text'));
+        $oMainPart->addElement(new GDO_FieldType('theme_projet', $theme['Theme']['libelle'], 'text'));
         $oMainPart->addElement(new GDO_FieldType('critere_trie_theme', $theme['Theme']['order'], 'text'));
     }
 
 }
+
 ?>
