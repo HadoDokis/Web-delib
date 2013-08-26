42a09b6980fce99fd591abcb0fbe59a2571b91aa
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-08-26 09:05:43 +0000

    mise en forme des source

--- branches/4.2/cake_webdelib/Model/Commentaire.php
+++ branches/4.2/cake_webdelib/Model/Commentaire.php
@@ -1,89 +1,87 @@
 <?php
 
-    class Commentaire extends AppModel {
+class Commentaire extends AppModel {
 
-        var $name = 'Commentaire';
-        var $validate = array( 'texte' => array(
-                                          array( 'rule'    => 'notEmpty',
-                                                 'message' => 'Entrer un commentaire.')));
+    var $name = 'Commentaire';
+    var $validate = array('texte' => array(
+            array('rule' => 'notEmpty',
+                'message' => 'Entrer un commentaire.')));
 
-   // -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
 
-		/**
-		 * Lecture des enregistrements
-		 *
-		 * @return array
-		 */
-		public function gedoooReadAll( $deliberation_id ) {
-            return $this->find(
-                'all',
-                array(
+    /**
+     * Lecture des enregistrements
+     *
+     * @return array
+     */
+    public function gedoooReadAll($deliberation_id) {
+        return $this->find('all', array(
                     'fields' => array('texte', 'commentaire_auto'),
                     'conditions' => array('Commentaire.delib_id' => $deliberation_id),
                     'recursive' => -1
-                )
-            );
-        }
+        ));
+    }
 
-		/**
-		 * Normalisation des enregistrements se trouvant sous la clé 'Commentaire':
-         * ajout des valeurs calculées, ...
-		 *
-		 * @param array $record
-		 * @return array
-		 */
-		public function gedoooNormalizeAll( array $data ) {
-            $commentaires = $data['Commentaires'];
-            unset( $data['Commentaires'] );
+    /**
+     * Normalisation des enregistrements se trouvant sous la clé 'Commentaire':
+     * ajout des valeurs calculées, ...
+     *
+     * @param array $record
+     * @return array
+     */
+    public function gedoooNormalizeAll(array $data) {
+        $commentaires = $data['Commentaires'];
+        unset($data['Commentaires']);
 
-            $listeCommentaires = array();
-            $listeAvisCommission = array();
+        $listeCommentaires = array();
+        $listeAvisCommission = array();
 
-            foreach( $commentaires as $commentaire ) {
-                if( $commentaire['Commentaire']['commentaire_auto'] ) {
-                    $listeAvisCommission[] = array( 'avis' => $commentaire['Commentaire']['texte'] );
-                }
-                else {
-                    $listeCommentaires[] = array( 'avis' => $commentaire['Commentaire']['texte'] );
-                }
+        foreach ($commentaires as $commentaire) {
+            if ($commentaire['Commentaire']['commentaire_auto']) {
+                $listeAvisCommission[] = array('avis' => $commentaire['Commentaire']['texte']);
+            } else {
+                $listeCommentaires[] = array('avis' => $commentaire['Commentaire']['texte']);
             }
-
-            if( !empty( $listeAvisCommission ) ) {
-                $data['AvisCommission'] = $listeAvisCommission;
-            }
-
-            if( !empty( $listeCommentaires ) ) {
-                $data['Commentaires'] = $listeCommentaires;
-            }
-
-            return $data;
         }
 
-		/**
-		 * Retourne une correspondance entre les champs CakePHP (même calculés)
-		 * et les champs Gedooo.
-		 *
-		 * @param array $records
-		 * @return array
-		 */
-		public function gedoooPaths() {
-			return array(
-                'avis' => 'texte',
-                'texte_commentaire' => 'texte',
-            );
+        if (!empty($listeAvisCommission)) {
+            $data['AvisCommission'] = $listeAvisCommission;
         }
 
-		/**
-		 * Retourne une correspondance entre les champs CakePHP (même calculés)
-		 * et les types Gedooo.
-		 *
-		 * @param array $records
-		 * @return array
-		 */
-		public function gedoooTypes() {
-			return array(
-                'texte' => 'text',
-            );
+        if (!empty($listeCommentaires)) {
+            $data['Commentaires'] = $listeCommentaires;
         }
+
+        return $data;
+    }
+
+    /**
+     * Retourne une correspondance entre les champs CakePHP (même calculés)
+     * et les champs Gedooo.
+     *
+     * @param array $records
+     * @return array
+     */
+    public function gedoooPaths() {
+        return array(
+            'avis' => 'texte',
+            'texte_commentaire' => 'texte',
+        );
+    }
+
+    /**
+     * Retourne une correspondance entre les champs CakePHP (même calculés)
+     * et les types Gedooo.
+     *
+     * @param array $records
+     * @return array
+     */
+    public function gedoooTypes() {
+        return array(
+            'texte' => 'text',
+        );
+    }
+
 }
+
 ?>
