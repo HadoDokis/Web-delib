b2a1702dc2c588e6918a60d4cb73f2f5b5c95c78
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-08-27 15:26:26 +0000

    ajout test liste presence

---
+++ branches/4.2/cake_webdelib/Test/Case/Model/ListepresenceTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Code source de la classe ListepresenceTest.
+ *
+ * @package app.Test.Case.Model
+ * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
+ */
+App::uses('Listepresence', 'Model');
+
+/**
+ * La classe ListepresenceTest ...
+ *
+ * @package app.Test.Case.Model
+ */
+class ListepresenceTest extends CakeTestCase {
+
+    /**
+     *
+     * @var Model
+     */
+    public $Listepresence = null;
+    
+    /**
+     * Fixtures utilisés.
+     *
+     * @var array
+     */
+    public $fixtures = array(
+        'app.Listepresence',
+        'app.Acteur',
+        'app.Typeacteur',
+        'app.Vote',
+    );
+
+    /**
+     * Préparation du test.
+     */
+    public function setUp() {
+        parent::setUp();
+        $this->Listepresence = ClassRegistry::init('Listepresence');
+    }
+
+    /**
+     * Nettoyage postérieur au test.
+     */
+    public function tearDown() {
+        unset($this->Listepresence);
+        parent::tearDown();
+    }
+
+    /**
+     * Test de la méthode Listepresence::gedoooReadAll()
+     */
+    public function testGedoooReadAll() {
+        // pour charger la fonction alias_querydata
+        include_once('../../plugins/Database/Config/bootstrap.php');
+        $result = $this->Listepresence->gedoooReadAll( 276 );
+//        debug($result);
+        
+        
+        
+        $this->markTestIncomplete('Ce test n\'a pas encoré été terminé.');
+    }
+
+}
+
+?>
