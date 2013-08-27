a8381c35eb93ea77e28484c418888f8689626e53
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-08-27 15:17:18 +0000

    ajout de données de test pour les votes

---
+++ branches/4.2/cake_webdelib/Test/Fixture/VoteFixture.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Code source de la classe VoteFixture.
+ *
+ * @package app.Test.Fixture
+ * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
+ */
+
+/**
+ * La classe VoteFixture ...
+ *
+ * @package app.Test.Fixture
+ */
+class VoteFixture extends CakeTestFixture {
+
+    /**
+     * On importe la définition de la table, pas les enregistrements.
+     *
+     * @var array
+     */
+    public $import = array(
+        'model' => 'Vote',
+        'records' => false
+    );
+
+    /**
+     * Définition des enregistrements.
+     *
+     * @var array
+     */
+    public $records = array(
+        array(
+            'id' => 790,
+            'acteur_id' => 1,
+            'delib_id' => 276,
+            'resultat' => 3,
+            'created' => '2013-08-19 17:01:43',
+            'modified' => '2013-08-19 17:01:43',
+        ),
+        array(
+            'id' => 791,
+            'acteur_id' => 2,
+            'delib_id' => 276,
+            'resultat' => 3,
+            'created' => '2013-08-19 17:01:43',
+            'modified' => '2013-08-19 17:01:43',
+        ),
+        array(
+            'id' => 792,
+            'acteur_id' => 3,
+            'delib_id' => 276,
+            'resultat' => 3,
+            'created' => '2013-08-19 17:01:43',
+            'modified' => '2013-08-19 17:01:43',
+        ),
+    );
+
+}
+
+?>
