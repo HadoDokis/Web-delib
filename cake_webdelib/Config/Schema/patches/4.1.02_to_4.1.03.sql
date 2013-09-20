34296730fd5eb3711759b26db3ab9cdb0cda4a50
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-09-20 14:00:31 +0000

    rajout de IF EXISTS dans le drop des colonnes zone_*

--- cake_webdelib/Config/Schema/patches/4.1.02_to_4.1.03.sql
+++ cake_webdelib/Config/Schema/patches/4.1.02_to_4.1.03.sql
@@ -7,15 +7,17 @@ ALTER TABLE seances ADD COLUMN numero_depot integer NOT NULL DEFAULT '0';
 
 ALTER TABLE deliberations ALTER COLUMN num_pref TYPE character varying(255);
 
-ALTER TABLE users   DROP COLUMN zone_1, 
-                    DROP COLUMN zone_2, 
-                    DROP COLUMN zone_3, 
-                    DROP COLUMN zone_4, 
-                    DROP COLUMN zone_5, 
-                    DROP COLUMN zone_6, 
-                    DROP COLUMN zone_7, 
-                    DROP COLUMN zone_8, 
-                    DROP COLUMN zone_9;
+ALTER TABLE users   DROP COLUMN IF EXISTS zone_1, 
+                    DROP COLUMN IF EXISTS zone_2, 
+                    DROP COLUMN IF EXISTS zone_3, 
+                    DROP COLUMN IF EXISTS zone_4, 
+                    DROP COLUMN IF EXISTS zone_5, 
+                    DROP COLUMN IF EXISTS zone_6, 
+                    DROP COLUMN IF EXISTS zone_7, 
+                    DROP COLUMN IF EXISTS zone_8, 
+                    DROP COLUMN IF EXISTS zone_9;
+
+ALTER TABLE collectivites ADD COLUMN logo bytea;
 
 --
 -- Fin du patch : fin de la transaction
