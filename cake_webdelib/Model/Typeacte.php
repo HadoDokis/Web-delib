4edb80aaa0323d27c06c5e8f7995db1546f82695
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-10-15 13:04:39 +0000

    déplacement de la méthode isDeletable et reformatage du code pour CakePHP

--- cake_webdelib/Model/Typeacte.php
+++ cake_webdelib/Model/Typeacte.php
@@ -1,86 +1,97 @@
 <?php
+
 class Typeacte extends AppModel {
 
-	var $name = 'Typeacte';
+    var $name = 'Typeacte';
+    var $displayField = 'libelle';
+    var $validate = array(
+        'libelle' => array(
+            array(
+                'rule' => 'notEmpty',
+                'message' => 'Entrer le libellé.'
+            ),
+            array(
+                'rule' => 'isUnique',
+                'message' => 'Entrez un autre libellé, celui-ci est déjà utilisé.'
+            )
+        ),
+        'compteur_id' => array(
+            array(
+                'rule' => 'notEmpty',
+                'message' => 'Sélectionner un compteur'
+            )
+        ),
+        'modelprojet_id' => array(
+            array(
+                'rule' => 'notEmpty',
+                'message' => 'Sélectionner le modèle de la projet'
+            )
+        ),
+        'modeldeliberation_id' => array(
+            array(
+                'rule' => 'notEmpty',
+                'message' => 'Sélectionner le modèle de délibération'
+            )
+        ),
+        'nature' => array(
+            array(
+                'rule' => 'notEmpty',
+                'message' => 'Selectionnez au moins une nature'
+            )
+        )
+    );
+    var $belongsTo = array(
+        'Compteur' => array(
+            'className' => 'Compteur',
+            'foreignKey' => 'compteur_id'),
+        'Nature' => array(
+            'className' => 'Nature',
+            'foreignKey' => 'nature_id'),
+        'Modelprojet' => array(
+            'className' => 'Model',
+            'foreignKey' => 'modeleprojet_id'),
+        'Modeldeliberation' => array(
+            'className' => 'Model',
+            'foreignKey' => 'modelefinal_id'),
+    );
+    var $hasMany = array('Deliberation');
 
-	var $displayField = 'libelle';
+    function getLibelle($type_id) {
+        $libelle = $this->find('first', array('conditions' => array('Typeacte.id' => $type_id),
+            'recursive' => -1,
+            'fields' => array('Typeacte.libelle')));
+        return $libelle['Typeacte']['libelle'];
+    }
 
-	var $validate = array(
-			'libelle' => array(
-					array(
-							'rule' => 'notEmpty',
-							'message' => 'Entrer le libellé.'
-					),
-					array(
-							'rule' => 'isUnique',
-							'message' => 'Entrez un autre libellé, celui-ci est déjà utilisé.'
-					)
-			),
-			'compteur_id' => array(
-					array(
-							'rule' => 'notEmpty',
-							'message' => 'Sélectionner un compteur'
-					)
-			),
-			'modelprojet_id' => array(
-					array(
-							'rule' => 'notEmpty',
-							'message' => 'Sélectionner le modèle de la projet'
-					)
-			),
-			'modeldeliberation_id' => array(
-					array(
-							'rule' => 'notEmpty',
-							'message' => 'Sélectionner le modèle de délibération'
-					)
-			),
-			'nature' => array(
-					array(
-							'rule' => 'notEmpty',
-							'message' => 'Selectionnez au moins une nature'
-					)
-			)
-	);
+    function getModelId($type_id, $field) {
+        $libelle = $this->find('first', array('conditions' => array('Typeacte.id' => $type_id),
+            'recursive' => -1,
+            'fields' => array($field)));
+        return $libelle['Typeacte'][$field];
+    }
 
-	var $belongsTo = array(
-			'Compteur' => array(
-					'className'  => 'Compteur',
-					'foreignKey' => 'compteur_id'),
-                        'Nature' => array(
-                                        'className'  => 'Nature',
-                                        'foreignKey' => 'nature_id'),
-			'Modelprojet' => array(
-					'className'  => 'Model',
-					'foreignKey' => 'modeleprojet_id'),
-			'Modeldeliberation' => array(
-					'className'  => 'Model',
-					'foreignKey' => 'modelefinal_id'),
-	);
+    function getIdDesNaturesDelib() {
+        $natures = $this->Nature->find('all', array('conditions' => array('Nature.code' => 'DE'),
+            'fields' => array('Nature.id'),
+            'recursive' => -1));
+        $typeactes = $this->find('all', array('conditions' => array('Typeacte.nature_id' => Set::extract('/Nature/id', $natures)),
+            'fields' => array('Typeacte.id'),
+            'recursive' => -1));
+        return Set::extract('/Typeacte/id', $typeactes);
+    }
 
-       var $hasMany = array('Deliberation');
+    /**
+     * Test la possibilité de supprimer un type d'acte (le type est il affécté à un acte ?)
+     * @param integer $id identifiant du type d'acte à éliminer
+     * @return boolean true si aucun acte n'est associée à ce type d'acte, false sinon
+     */
+    function isDeletable($id) {
+        $nbSeancesEnCours = $this->Deliberation->find('count', array(
+            'conditions' => array('typeacte_id' => $id)
+        ));
+        return empty($nbSeancesEnCours);
+    }
 
-	function getLibelle ($type_id) {
-		$libelle = $this->find('first', array('conditions' => array('Typeacte.id' => $type_id),
-                                                      'recursive'  => -1,
-                                                      'fields'     => array('Typeacte.libelle')));
-		return $libelle['Typeacte']['libelle'];
-	}
-
-        function getModelId ($type_id, $field) {
-            $libelle = $this->find('first', array('conditions' => array('Typeacte.id' => $type_id),
-                                                  'recursive'  => -1,
-                                                  'fields'     => array($field)));
-            return $libelle['Typeacte'][$field];
-        }
- 
-        function getIdDesNaturesDelib() {
-            $natures = $this->Nature->find('all', array('conditions' => array('Nature.code' => 'DE'), 
-                                                        'fields'     => array('Nature.id'),
-                                                        'recursive'  => -1)); 
-            $typeactes = $this->find('all', array('conditions' => array('Typeacte.nature_id' => Set::extract('/Nature/id', $natures)),
-                                                  'fields'     => array('Typeacte.id'), 
-                                                  'recursive'  => -1));
-            return Set::extract('/Typeacte/id', $typeactes);
-        }
 }
+
 ?>
