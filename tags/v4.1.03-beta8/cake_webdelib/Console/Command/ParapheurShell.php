589e3a36d4512988d741fd3bfd7ff95632e22c3e
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-11-14 14:03:39 +0000

    Création du tag 4.1.03-beta8

---
+++ tags/v4.1.03-beta8/cake_webdelib/Console/Command/ParapheurShell.php
@@ -0,0 +1,117 @@
+<?php
+
+App::import(array('Model', 'AppModel', 'File'));
+App::import(array('Model', 'Deliberation', 'File'));
+App::import(array('Model', 'Commentaire', 'File'));
+
+class ParapheurShell extends Shell {
+
+    var $uses = array('Deliberation', 'Commentaire');
+
+    function startup() {
+
+    }
+
+    function main() {
+
+        App::uses('AppShell', 'Console/Command');
+        App::uses('ComponentCollection', 'Controller');
+        App::uses('IparapheurComponent', 'Controller/Component');
+        
+        //Si service désactivé ==> quitter
+        if (!Configure::read('USE_PARAPH')) {
+            $this->out("Service i-Parapheur désactivé");
+            exit;
+        }
+        $collection = new ComponentCollection();
+        $this->Parafwebservice = new IparapheurComponent($collection);
+        // Controle de l'avancement des délibérations dans le parapheur
+        $delibs = $this->Deliberation->find('all', array('conditions' => array('Deliberation.etat >' => 2,
+                'Deliberation.etat_parapheur' => 1),
+            'recursive' => -1,
+            'fields' => array('id', 'objet', 'typeacte_id')));
+
+        foreach ($delibs as $delib) {
+            $compteur_id = 0;
+            $typeacte = $this->Deliberation->Typeacte->find('first', array('conditions' => array('Typeacte.id' => $delib['Deliberation']['typeacte_id'])));
+            if ($typeacte['Nature']['code'] != 'DE')
+                $compteur_id = $typeacte['Typeacte']['compteur_id'];
+            $objetDossier = $this->Parafwebservice->handleObject($delib['Deliberation']['objet']);
+            $this->_checkEtatParapheur($delib['Deliberation']['id'], $objetDossier, false, $compteur_id);
+        }
+    }
+
+    function _checkEtatParapheur($delib_id, $objet, $tdt = false, $compteur_id = 0) {
+        App::uses('AppShell', 'Console/Command');
+        App::uses('ComponentCollection', 'Controller');
+        App::uses('IparapheurComponent', 'Controller/Component');
+        $collection = new ComponentCollection();
+        $this->Parafwebservice = new IparapheurComponent($collection);
+        
+        $this->Deliberation->id = $delib_id;
+        $delib = $this->Deliberation->find('first', array('conditions' => array("Deliberation.id" => $delib_id), 'recursive' => -1));
+        
+        $id_dossier = $delib['Deliberation']['id_parapheur'];
+        if ($delib['Deliberation']['id_parapheur'] != "")
+                $id_dossier = $delib['Deliberation']['id_parapheur'];
+        else //DEPRECATED (rétro-compatibilité vieux dossiers parapheur)
+                $id_dossier = "$delib_id $objet";
+
+        $histo = $this->Parafwebservice->getHistoDossierWebservice($id_dossier);
+        if (isset($histo['logdossier'])){
+        for ($i = 0; $i < count($histo['logdossier']); $i++) {
+            if (!$tdt) {
+                if (($histo['logdossier'][$i]['status'] == 'Signe') ||
+                        ($histo['logdossier'][$i]['status'] == 'Archive')) {
+
+                    // TODO LIST : Récupère la date et heure de signature  + QUi l'a signé (annotation)
+                    $this->Commentaire->create();
+                    $comm ['Commentaire']['delib_id'] = $delib_id;
+                    $comm ['Commentaire']['agent_id'] = -1;
+                    $comm ['Commentaire']['texte'] = $histo['logdossier'][$i]['nom'] . " : " . $histo['logdossier'][$i]['annotation'];
+                    $comm ['Commentaire']['commentaire_auto'] = 0;
+                    $this->Commentaire->save($comm['Commentaire']);
+
+                    if ($delib['Deliberation']['etat_parapheur'] == 1) {
+                        if ($histo['logdossier'][$i]['status'] == 'Signe') {
+                            $dossier = $this->Parafwebservice->GetDossierWebservice($id_dossier);
+                            if (!empty($dossier['getdossier'][10])) {
+                                $this->Deliberation->saveField('delib_pdf', base64_decode($dossier['getdossier'][8]));
+                                $this->Deliberation->saveField('signature', base64_decode($dossier['getdossier'][10]));
+                            }
+                            $this->Deliberation->saveField('signee', 1);
+                            /*
+                            if ($compteur_id != 0) {
+                                $this->Deliberation->saveField('date_acte', date("Y-m-d H:i:s", strtotime("now")));
+                                $num = $this->Deliberation->Seance->Typeseance->Compteur->genereCompteur($compteur_id);
+                                $this->Deliberation->saveField('num_delib', $num);
+                            }
+                            */
+                        }
+                        // etat_paraph à 1, donc, nous sommes en post_seance, on ne supprime pas le projet
+                        $this->Deliberation->saveField('etat_parapheur', 2);
+                        $this->Parafwebservice->archiverDossierWebservice($id_dossier, "EFFACER");
+                    }
+                } elseif (($histo['logdossier'][$i]['status'] == 'RejetSignataire') ||
+                        ($histo['logdossier'][$i]['status'] == 'RejetVisa')) { // Cas de refus dans le parapheur
+                    $this->Commentaire->create();
+                    $comm ['Commentaire']['delib_id'] = $delib_id;
+                    $comm ['Commentaire']['agent_id'] = -1;
+                    $comm ['Commentaire']['texte'] = $histo['logdossier'][$i]['nom'] . " : " . $histo['logdossier'][$i]['annotation'];
+                    $comm ['Commentaire']['commentaire_auto'] = 0;
+                    $this->Commentaire->save($comm['Commentaire']);
+                    $this->Deliberation->saveField('etat_parapheur', -1);
+                    // Supprimer le dossier du parapheur
+                    $this->Parafwebservice->effacerDossierRejeteWebservice($id_dossier);
+                }
+            } else {
+                if ($histo['logdossier'][$i]['status'] == 'EnCoursTransmission')
+                    return true;
+            }
+        }}
+        return false;
+    }
+
+}
+
+?>
---
+++ tags/v4.1.03-beta8/cake_webdelib/Test/Case/Controller/Components/ConversionComponentTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+* Code source de la classe Histochoixcer93Test.
+*
+* PHP 5.3
+*
+* @package app.Test.Case.Model
+* @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
+*/
+App::uses('ComponentCollection', 'Controller');
+App::uses( 'ConversionComponent', 'Controller/Component' );
+
+/**
+* Classe Histochoixcer93Test.
+*
+* @package app.Test.Case.Model
+* 
+*/
+
+class ConversionComponentTest extends CakeTestCase {
+    public $ConversionComponent = null;
+    public $Controller = null;
+    
+
+    public function setUp() {
+        parent::setUp();
+        $Collection = new ComponentCollection();
+        $this->ConversionComponent = new ConversionComponent($Collection);
+    }
+    /**
+    * Méthode exécutée avant chaque test.
+    *
+    * @return void
+    */
+    public function tearDown() {
+        parent::tearDown();
+        unset( $this->ConversionComponent );
+        unset($this->Controller);
+    }
+
+        /**
+    * Méthode exécutée après chaque test.
+    *
+    * @return void
+    */
+    public function testconvertirFlux(){
+        
+        $file=new File(WWW_ROOT.DS.'check'.DS.'files'.DS.'checkConversion.odt');
+        $file->close();
+        $result=$this->ConversionComponent->convertirFlux($file->read(), 'pdf');
+        $file=new File(TMP.DS.'files'.DS.'checkConversion.pdf',TRUE);
+        $file->append($result);
+        $expected = $file->mime();
+        $file->close(); 
+        
+        $this->assertEquals($expected, 'application/pdf');
+    }
+
+
+}
+
+?>
