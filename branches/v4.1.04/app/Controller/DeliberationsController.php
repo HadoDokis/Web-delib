cd50e30bc39dae2aa2a06c1b8533f949ed73139d
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-11-25 17:16:46 +0000

    Correction : Ne prendre en compte que les actifs dans la liste des services emetteurs (generateTreeList)

--- branches/v4.1.04/app/Controller/DeliberationsController.php
+++ branches/v4.1.04/app/Controller/DeliberationsController.php
@@ -2830,7 +2830,7 @@ class DeliberationsController extends AppController {
             $this->set('rapporteurs', $this->Acteur->generateListElus());
             //  $this->set('selectedRapporteur', $this->data['Deliberation']['rapporteur_id']);
             $this->set('date_seances', $this->Seance->generateAllList());
-            $this->set('services', $this->Deliberation->Service->generateTreeList(null, null, null, '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'));
+            $this->set('services', $this->Deliberation->Service->generateTreeList(array('actif'=>1), null, null, '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'));
             $this->set('themes', $this->Deliberation->Theme->find('list', array('order' => array('libelle asc'),
                         'recursive' => -1,
                         'fields' => array('Theme.id', 'Theme.libelle'))));
@@ -2940,7 +2940,7 @@ class DeliberationsController extends AppController {
             $this->set('rapporteurs', $this->Acteur->generateListElus());
             //    $this->set('selectedRapporteur', $this->data['Deliberation']['rapporteur_id']);
             $this->set('date_seances', $this->Seance->generateAllList());
-            $this->set('services', $this->Deliberation->Service->generateTreeList(null, null, null, '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'));
+            $this->set('services', $this->Deliberation->Service->generateTreeList(array('actif'=>1), null, null, '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'));
             $this->set('themes', $this->Deliberation->Theme->find('list', array('order' => array('libelle asc'),
                         'recursive' => -1,
                         'fields' => array('Theme.id', 'Theme.libelle'))));
