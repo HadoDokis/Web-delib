ac56457417606e1a0241eb63a4c452b2e39b40e6
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-12-10 11:02:19 +0000

    changement numéro de version

--- app/Config/core.php
+++ app/Config/core.php
@@ -305,7 +305,7 @@ Cache::config('_cake_model_', array(
     //Configure::write('Config.language', 'fra');
     
     require_once ('webdelib.inc');
-    define('VERSION', '4.1.03');
+    define('VERSION', '4.1.04-beta1');
     //appVersion
     //dbVersion
     include_once(ROOT.DS.APP_DIR.DS.'Plugin'.DS.'Cakeflow'.DS.'Config'.DS.'cakeflow.conf.php');
