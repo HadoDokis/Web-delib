4f859aec6badda83d65244e4da01955cc4bf5058
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-12-19 09:47:17 +0000

    Suppresion du tag

--- tags/v4.1.02-rc3/Model/Ado.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once(ROOT.DS.'lib/Cake/Model/Aco.php');
-    class Ado extends Aco {
-       var $name = 'Ado';
-       var $useTable = "ados";
-    }
-?>
--- tags/v4.1.02-rc3/Model/Deliberationseance.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Deliberationseance extends AppModel {
-    var $name = 'Deliberationseance';
-    var $useTable = 'deliberations_seances';
-    var $belongsTo = array('Deliberation', 'Seance');
-}
-?>
