e159c3a0b6e8636e81824b4a88c2031a949e619c
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2013-12-19 09:42:32 +0000

    Suppression du tag

--- tags/v0.9.6-2_beta/cake_webdelib/models/collectivite.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Collectivite extends AppModel {
-	var $name = 'Collectivite';
-	var $cacheSources = 'false';
-}
-?>
--- tags/v0.9.6-2_beta/cake_webdelib/plugins/acm/models/acm_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-class AcmUser extends AcmAppModel
-{
-	var $name = 'AcmUser';
-	var $useTable = false;
-
-
-}
-?>
