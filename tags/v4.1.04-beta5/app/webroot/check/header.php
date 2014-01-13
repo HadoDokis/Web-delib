0f985a78bbc8695587a5f07fb8615f69fe13cf24
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-01-13 15:58:55 +0000

    Création du tag 4.1.04-beta5

---
+++ tags/v4.1.04-beta5/app/webroot/check/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="fr-FR">
+<head> 
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="scripts/expand.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    // --- first section initially expanded:
+		    $("h2.expand").toggler({initShow: "div.collapse:first"});
+		    $("#content").expandAll({trigger: "h2.expand", ref: "div.demo",  speed: 300, oneSwitch: false});
+		});
+	</script>
+	<!--<![endif]-->
+	<title>Webdelib check</title>
+	<link rel="stylesheet" type="text/css" href="css/check.css" />
+	<link rel="stylesheet" type="text/css" href="css/effect.css" />
+	<div id="headerLogoContainer">
+		<img alt="" src="../img/wavedelib.png">
+	</div>
+</head> 
+<body>
