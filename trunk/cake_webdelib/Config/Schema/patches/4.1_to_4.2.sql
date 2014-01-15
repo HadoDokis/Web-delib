4a58ceb32691ec010f80d7993fa146998c41f570
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-01-15 14:16:01 +0000

    suppression ajout colonne modeltype_id (déplacement vers sql ModelOdtValidator)

--- trunk/cake_webdelib/Config/Schema/patches/4.1_to_4.2.sql
+++ trunk/cake_webdelib/Config/Schema/patches/4.1_to_4.2.sql
@@ -1,7 +1,6 @@
 BEGIN;
 
 -- Mise à jour de la table models
-ALTER TABLE models ADD COLUMN modeltype_id INTEGER REFERENCES modeltypes(id) DEFAULT 1;
 ALTER TABLE models RENAME COLUMN name TO filename;
 ALTER TABLE models RENAME COLUMN modele TO name;
 ALTER TABLE models RENAME COLUMN size TO filesize;
@@ -18,6 +17,7 @@ ALTER TABLE models RENAME TO modeltemplates;
 -- Nouvelles notifications utilisateur
 ALTER TABLE users ADD COLUMN mail_modif_projet_cree BOOLEAN DEFAULT FALSE;
 ALTER TABLE users ADD COLUMN mail_modif_projet_valide BOOLEAN DEFAULT FALSE;
+ALTER TABLE users ADD COLUMN mail_retard_validation BOOLEAN DEFAULT FALSE;
 
 -- Mise à jour : joindre les annexes par défaut
 UPDATE annexes SET joindre_fusion=TRUE;
@@ -32,5 +32,26 @@ ALTER TABLE deliberations ADD COLUMN president_id INTEGER DEFAULT NULL;
 
 -- Télé-transmissible
 ALTER TABLE typeactes ADD COLUMN teletransmettre BOOL DEFAULT TRUE;
+
+-- Crons
+ALTER TABLE crons DROP COLUMN controller;
+ALTER TABLE crons ADD COLUMN model VARCHAR DEFAULT 'CronJob';
+
+-- Tâche planifiée de mise à jour des délégations
+UPDATE crons set action='delegationJob', plugin=null WHERE id=1;
+
+-- Tâche planifiée de déclenchement des alertes de retard (workflow)
+INSERT INTO crons (id, nom, description, plugin, model, action, has_params, params, next_execution_time, execution_duration, last_execution_start_time, last_execution_end_time, last_execution_report, last_execution_status, active, created, created_user_id, modified, modified_user_id)
+  VALUES ('2', 'Circuits de traitement : Déclenchement des alertes de retard', 'Relance les utilisateurs devant valider un projet en retard', null, 'CronJob',	'retardCakeflowJob',	'f', null, now(), 'P1D', now(), now(), 'Cette tâche n''a encore jamais été exécutée.', null,	true, now(), '1', now(), '1');
+
+-- Tâche planifiée de mise à jour du status des actes envoyés au i-parapheur
+INSERT INTO crons (id, nom, description, plugin, model, action, has_params, params, next_execution_time, execution_duration, last_execution_start_time, last_execution_end_time, last_execution_report, last_execution_status, active, created, created_user_id, modified, modified_user_id)
+  VALUES ('3', 'Signature : Mise à jour de l''etat des dossiers envoyés au i-Parapheur', 'Met à jour l''état des projets envoyés au i-Parapheur pour signature et rapatrie les informations de ceux en fin de circuit.', null, 'CronJob',	'signatureJob',	'f', null, now(), 'P1D', now(), now(), 'Cette tâche n''a encore jamais été exécutée.', null,	true, now(), '1', now(), '1');
+
+-- Tâche planifiée de mise à jour du status des actes envoyés au i-parapheur
+INSERT INTO crons (id, nom, description, plugin, model, action, has_params, params, next_execution_time, execution_duration, last_execution_start_time, last_execution_end_time, last_execution_report, last_execution_status, active, created, created_user_id, modified, modified_user_id)
+  VALUES ('4', 'S2low : mise à jours des échanges', 'Envoi/Réception des courriers S2low', null, 'CronJob',	's2lowJob',	'f', null, now(), 'P1D', now(), now(), 'Cette tâche n''a encore jamais été exécutée.', null,	true, now(), '1', now(), '1');
+
+ALTER TABLE crons ADD COLUMN lock BOOL DEFAULT FALSE;
 
 COMMIT;
