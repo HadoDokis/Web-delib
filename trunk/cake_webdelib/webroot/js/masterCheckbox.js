e56c5a518faa5525dd797f84236233e04770ad62
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-01-23 12:54:38 +0000

    bugfix master checkbox - checkbox disabled

--- trunk/cake_webdelib/webroot/js/masterCheckbox.js
+++ trunk/cake_webdelib/webroot/js/masterCheckbox.js
@@ -6,16 +6,16 @@ $(document).ready(function () {
     // masterCheckbox -> Checkbox
     // La master checkbox coche/décoche tout d'un coup
     $("#masterCheckbox").change(function () {
-        $("input[type=checkbox]").not(":disabled").prop('checked', $(this).prop('checked'));
+        $("input[type=checkbox]").filter(':not(:disabled)').prop('checked', $(this).prop('checked'));
     });
     //Checkbox -> masterCheckbox
-    $('input[type="checkbox"]').change(function () {
-        var checked = ($('input[type="checkbox"]').not('#masterCheckbox').filter(':not(:checked)').length === 0);
+    $('input[type="checkbox"]').not("#masterCheckbox").change(function () {
+        var checked = ($('input[type="checkbox"]').not('#masterCheckbox').filter(':not(:disabled)').filter(':not(:checked)').length === 0);
         $('#masterCheckbox').prop('checked', checked);
     });
     // Si toutes les checkboxes (sauf masterCheckbox) sont cochées, cocher masterCheckbox
-    $("#masterCheckbox").prop('checked', $("input[type=checkbox]").not("#masterCheckbox").prop('checked'));
-    $("#masterCheckbox").prop('disabled', !$("input[type=checkbox]").not(':disabled').not("#masterCheckbox").length);
+    $("#masterCheckbox").prop('checked', $("input[type=checkbox]").filter(':not(:disabled)').not("#masterCheckbox").prop('checked'));
+    $("#masterCheckbox").prop('disabled', !$("input[type=checkbox]").filter(':not(:disabled)').not("#masterCheckbox").length);
 });
 
 function selectAll() {
