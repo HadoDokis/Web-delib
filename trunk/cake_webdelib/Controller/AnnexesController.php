7fe13d06c9cc5a7ea0f5d87800a783adf3c78fca
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-01-31 15:27:55 +0000

    mise en place d'une nouvelle propriété de controller ($this->previous) contenant l'url de la page (vraiment) précédente

--- trunk/cake_webdelib/Controller/AnnexesController.php
+++ trunk/cake_webdelib/Controller/AnnexesController.php
@@ -1,51 +1,52 @@
 <?php
+
 class AnnexesController extends AppController {
 
-	// Gestion des droits
-	var $aucunDroit;
+    // Gestion des droits
+    public $aucunDroit;
 
-	function delete($id = null) {
-		if ($this->Annex->del($id)) {
-		    $this->redirect($this->Session->read('user.User.lasturl'));
-		}
-	}
+    function delete($id = null) {
+        if ($this->Annex->del($id)) {
+            $this->Session->setFlash('Annexe supprimée', 'growl');
+        } else {
+            $this->Session->setFlash('Impossible de supprimer cette annexe', 'growl');
+        }
+        $this->redirect($this->previous);
+    }
 
-	function download($id=null, $type=''){
-                $DOC_TYPE = Configure::read('DOC_TYPE');
-            
-		// lecture en base
-		$annexe = $this->Annex->find('first', array(
-                        'fields'=> 'data,data_pdf,filename,filename_pdf,filetype',
-			'recursive' => -1,
-			'conditions' => array('id'=>$id)));
-                
-                if ($type == 'odt') {
-		    $content  = $annexe['Annex']['data'];
-                    $filename = $annexe['Annex']['filename'];
-                    $type     = 'application/vnd.oasis.opendocument.text';
-                }
-                elseif ($type == 'pdf') {
-		    $content  = $annexe['Annex']['data_pdf'];
-                    $filename = $annexe['Annex']['filename_pdf'];
-                    $type     = 'application/pdf';
-                }
-                elseif ($DOC_TYPE[$annexe['Annex']['filetype']]['extention']!='pdf') {
-		    $content  = $annexe['Annex']['data'];
-                    $filename = $annexe['Annex']['filename'];
-                    $type     = $annexe['Annex']['filetype'];
-                }
-                else {
-		    $content  = $annexe['Annex']['data_pdf'];
-                    $filename = $annexe['Annex']['filename_pdf'];
-                    $type     = $annexe['Annex']['filetype'];
-                }
+    function download($id = null, $type = '') {
+        $DOC_TYPE = Configure::read('DOC_TYPE');
 
-		if (empty($annexe)) return;
-		header('Content-type: '.$type);
-		header('Content-Length: '.strlen($content));
-		header('Content-Disposition: attachment; filename="'.$filename.'"');
-                die($content);
-	}
+        // lecture en base
+        $annexe = $this->Annex->find('first', array(
+            'fields' => 'data,data_pdf,filename,filename_pdf,filetype',
+            'recursive' => -1,
+            'conditions' => array('id' => $id)));
+
+        if ($type == 'odt') {
+            $content = $annexe['Annex']['data'];
+            $filename = $annexe['Annex']['filename'];
+            $type = 'application/vnd.oasis.opendocument.text';
+        } elseif ($type == 'pdf') {
+            $content = $annexe['Annex']['data_pdf'];
+            $filename = $annexe['Annex']['filename_pdf'];
+            $type = 'application/pdf';
+        } elseif ($DOC_TYPE[$annexe['Annex']['filetype']]['extention'] != 'pdf') {
+            $content = $annexe['Annex']['data'];
+            $filename = $annexe['Annex']['filename'];
+            $type = $annexe['Annex']['filetype'];
+        } else {
+            $content = $annexe['Annex']['data_pdf'];
+            $filename = $annexe['Annex']['filename_pdf'];
+            $type = $annexe['Annex']['filetype'];
+        }
+
+        if (empty($annexe)) return;
+        header('Content-type: ' . $type);
+        header('Content-Length: ' . strlen($content));
+        header('Content-Disposition: attachment; filename="' . $filename . '"');
+        echo $content;
+        exit;
+    }
 
 }
-?>
