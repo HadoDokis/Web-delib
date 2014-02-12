c3375d74d38f31c147631b68739dc862c25bb5a0
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-02-12 14:05:34 +0000

    nouvelle fonction setVariablesFusion() : fusion des variables Gedooo pour les acteurs

--- trunk/cake_webdelib/Model/Acteur.php
+++ trunk/cake_webdelib/Model/Acteur.php
@@ -163,5 +163,47 @@ class Acteur extends AppModel
                         $oMainPart->addElement(new GDO_FieldType("note_$alias",           ($acteur[$this->alias]['note']),       'text'));
                 }
         }
+
+    /**
+     * fonction d'initialisation des variables de fusion pour l'allias utilisé pour la liaison (Rapporteur, President, ...)
+     * les bibliothèques Gedooo doivent être inclues par avance
+     * génère une exception en cas d'erreur
+     * @param object_by_ref $oMainPart variable Gedooo de type maintPart du document à fusionner
+     * @param integer $id id du modèle lié
+     * @param objet_by_ref $modelOdtInfos objet PhpOdtApi du fichier odt du modèle d'édition
+     * @param string $suffixe suffixe des variables de fusion
+     */
+    function setVariablesFusion(&$oMainPart, $id, &$modelOdtInfos, $suffixe='') {
+        // initialisations
+        if (empty($suffixe))
+            $suffixe = trim(strtolower($this->alias));
+        $fields = array();
+        if ($modelOdtInfos->hasUserField('salutation_'.$suffixe)) $fields[]= 'salutation';
+        if ($modelOdtInfos->hasUserField('prenom_'.$suffixe)) $fields[]= 'prenom';
+        if ($modelOdtInfos->hasUserField('nom_'.$suffixe)) $fields[]= 'nom';
+        if ($modelOdtInfos->hasUserField('titre_'.$suffixe)) $fields[]= 'titre';
+        if ($modelOdtInfos->hasUserField('position_'.$suffixe)) $fields[]= 'position';
+        if ($modelOdtInfos->hasUserField('email_'.$suffixe)) $fields[]= 'email';
+        if ($modelOdtInfos->hasUserField('telmobile_'.$suffixe)) $fields[]= 'telmobile';
+        if ($modelOdtInfos->hasUserField('telfixe_'.$suffixe)) $fields[]= 'telfixe';
+        if ($modelOdtInfos->hasUserField('date_naissance_'.$suffixe)) $fields[]= 'date_naissance';
+        if ($modelOdtInfos->hasUserField('adresse1_'.$suffixe)) $fields[]= 'adresse1';
+        if ($modelOdtInfos->hasUserField('adresse2_'.$suffixe)) $fields[]= 'adresse2';
+        if ($modelOdtInfos->hasUserField('cp_'.$suffixe)) $fields[]= 'cp';
+        if ($modelOdtInfos->hasUserField('ville_'.$suffixe)) $fields[]= 'ville';
+        if ($modelOdtInfos->hasUserField('note_'.$suffixe)) $fields[]= 'note';
+        if (empty($fields)) return;
+
+        // lecture en base de données
+        $acteur = $this->find('first', array(
+            'recursive' => -1,
+            'fields' => $fields,
+            'conditions' => array('id' => $id)));
+        if (empty($acteur))
+            throw new Exception('acteur '.$suffixe.' id:'.$id.' non trouvé en base de données');
+        foreach($acteur[$this->alias] as $field => $val)
+            $oMainPart->addElement(new GDO_FieldType($field.'_'.$suffixe, $val, 'text'));
+    }
+
 }
 ?>
