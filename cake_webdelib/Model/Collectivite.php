a2432f4372dd41d70a04840df140823aa1c3f756
florentveyres <florentveyres@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-02-12 14:11:06 +0000

    nouvelle fonction setVariablesFusion() : fusion des variables Gedooo des annexes

--- cake_webdelib/Model/Collectivite.php
+++ cake_webdelib/Model/Collectivite.php
@@ -73,6 +73,37 @@ class Collectivite extends AppModel {
         return $logo_path;
     }
 
+    /**
+     * fonction d'initialisation des variables de fusion pour la collectivité
+     * les bibliothèques Gedooo doivent être inclues par avance
+     * génère une exception en cas d'erreur
+     * @param object_by_ref $oMainPart variable Gedooo de type maintPart du document à fusionner
+     * @param integer $dataId id des données à fusionner
+     * @param objet_by_ref $modelOdtInfos objet PhpOdtApi du fichier odt du modèle d'édition
+     */
+    function setVariablesFusion(&$oMainPart, $dataId, &$modelOdtInfos) {
+        // lecture de la collectivité en  base de données
+        $collectivite = $this->find('first', array(
+            'recursive'  => -1,
+            'fields' => array('id', 'nom', 'adresse', 'CP', 'ville', 'telephone'),
+            'conditions' => array('id' => $dataId)));
+        if (empty($collectivite))
+            throw new Exception('collectivité id:'.$dataId.' non trouvé en base de données');
+
+        // initialisation des variables
+        if ($modelOdtInfos->hasUserField('nom_collectivite'))
+            $oMainPart->addElement(new GDO_FieldType('nom_collectivite', $collectivite['Collectivite']['nom'], "text"));
+        if ($modelOdtInfos->hasUserField('adresse_collectivite'))
+            $oMainPart->addElement(new GDO_FieldType('adresse_collectivite', $collectivite['Collectivite']['adresse'], "text"));
+        if ($modelOdtInfos->hasUserField('cp_collectivite'))
+            $oMainPart->addElement(new GDO_FieldType('cp_collectivite', $collectivite['Collectivite']['CP'], "text"));
+        if ($modelOdtInfos->hasUserField('ville_collectivite'))
+            $oMainPart->addElement(new GDO_FieldType('ville_collectivite', $collectivite['Collectivite']['ville'], "text"));
+        if ($modelOdtInfos->hasUserField('telephone_collectivite'))
+            $oMainPart->addElement(new GDO_FieldType('telephone_collectivite', $collectivite['Collectivite']['telephone'], "text"));
+    }
+
+
 }
 
 ?>
