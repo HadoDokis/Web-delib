e549ab37f9f89a92a677e16b07ac1c6088d89454
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-02-17 09:22:57 +0000

    correction lien retour page commentaire de projet

--- trunk/cake_webdelib/Controller/CommentairesController.php
+++ trunk/cake_webdelib/Controller/CommentairesController.php
@@ -6,9 +6,9 @@ class CommentairesController extends AppController
 
     public function add($delib_id = null)
     {
-        if (!$delib_id) {
-            $this->Session->setFlash('Invalide id pour la délibération du commentaire.', 'growl');
-            $this->redirect('/');
+        if (empty($delib_id)) {
+            $this->Session->setFlash('Identifiant de délibération introuvable.', 'growl');
+            return $this->redirect($this->previous);
         }
         $this->set('delib_id', $delib_id);
         if (!empty($this->request->data)) {
@@ -16,26 +16,27 @@ class CommentairesController extends AppController
             $this->request->data['Commentaire']['agent_id'] = $this->Session->read('user.User.id');
             $this->request->data['Commentaire']['commentaire_auto'] = 0;
             if ($this->Commentaire->save($this->request->data)) {
-                $this->redirect(array('controller' => 'deliberations', 'action' => 'traiter', $this->request->data['Commentaire']['delib_id']));
+                return $this->redirect($this->previous);
             } else {
                 $this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.', 'growl', array('type' => 'erreur'));
             }
         }
+        $this->set('retour', $this->previous);
     }
 
     public function edit($id = null, $delib_id = null)
     {
         if (empty($this->request->data)) {
-            if (!$id) {
+            if (empty($id)) {
                 $this->Session->setFlash('Invalide id pour le commentaire', 'growl');
-                $this->redirect(array('controller' => 'deliberations', 'action' => 'traiter', $delib_id));
+                return $this->redirect($this->previous);
             }
             $this->set('delib_id', $delib_id);
             $this->request->data = $this->Commentaire->read(null, $id);
         } else {
             $this->request->data['Commentaire']['agent_id'] = $this->Session->read('user.User.id');
             if ($this->Commentaire->save($this->request->data)) {
-                $this->redirect(array('controller' => 'deliberations', 'action' => 'traiter', $this->request->data['Commentaire']['delib_id']));
+                return $this->redirect($this->previous);
             } else {
                 $this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.', 'growl', array('type' => 'erreur'));
             }
