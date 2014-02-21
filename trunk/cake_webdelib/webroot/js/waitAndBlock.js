65ca274f51617215447aea15699d37e2790c2fa9
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-02-21 15:37:55 +0000

    passage de la requete de terminaison en synchrone

--- trunk/cake_webdelib/webroot/js/waitAndBlock.js
+++ trunk/cake_webdelib/webroot/js/waitAndBlock.js
@@ -3,7 +3,7 @@
  * Requires: jQuery v1.3+ & attendable.js
  * Copyright (c) 2013 Florian Ajir
  */
-$(document).ready(function() {
+$(document).ready(function () {
     $(".waiter").attendable();
     $("a.delib_pdf").attendable();
     $("a.link_clore_seance").attendable();
@@ -12,12 +12,13 @@ $(document).ready(function() {
 });
 
 function pauseWhileDownload(elt) {
-    console.log('pause');
-    console.log($(elt).attr('href'));
     var token = setToken();
     //Ajout du num de cookie en dernier argument url
-    if ($(elt).attr('href')){
-        $(elt).attr('href', $(elt).attr('href')+ "/" + token);
+    var href = $(elt).attr('href');
+    if (href) {
+        if (href.substr(href.length - 1) != '/')
+            href += '/';
+        $(elt).attr('href', href + token);
     }
     blockUI(elt, token); //Veuillez patienter pendant la génération du document
 }
@@ -30,7 +31,7 @@ function getCookie(name) {
 
 function expireCookie(cName) {
     document.cookie =
-            encodeURIComponent(cName) +
+        encodeURIComponent(cName) +
             "=deleted; expires=" +
             new Date(0).toUTCString();
 }
@@ -46,47 +47,45 @@ function setToken() {
 
 // Prevents double-submits by waiting for a cookie from the server.
 function blockUI(elt, token) {
-    var retourRequete=true;
+    var retourRequete = true;
     var downloadTimer;
 
-    downloadTimer = window.setInterval(function() {
-        if(retourRequete){
-            retourRequete=false;
+    downloadTimer = window.setInterval(function () {
+        if (retourRequete) {
+            retourRequete = false;
+            console.log('requete');
             $.ajax({
-            url: '/models/generationToken/'+token,
-            cache: false,
-            type: 'GET',
-            dataType: 'json',
-            success: function (response) {
-                try
-                {
-                    if (token.toString() === response.downloadToken) {
-                        window.clearInterval(downloadTimer);
-                        unblockUI(elt, token);
-                    }else
-                        retourRequete=true;
+                url: '/models/generationToken/' + token,
+                cache: false,
+                type: 'GET',
+                dataType: 'json',
+                async: false,
+                success: function (response) {
+                    try {
+                        if (token.toString() === response.downloadToken) {
+                            window.clearInterval(downloadTimer);
+                            unblockUI(elt, token);
+                        } else
+                            retourRequete = true;
+                    }
+                    catch (err) {
+                        alert(err);
+                        retourRequete = true;
+                    }
+                },
+                error: function(xhr, status, error) {
+                    retourRequete = true;
                 }
-                catch(err)
-                {
-                    alert(err);
-                    retourRequete=true;
-                }                
-            },
-            error: function(error)
-            {
-               alert(error);
-               unblockUI(elt, token);
-            }
             });
         }
-    }, 2000);
+    }, 1000);
 }
 
 function unblockUI(elt, token) {
     //Suppression du token de la fin d'url
-    if ($(elt).attr('href')){
+    if ($(elt).attr('href')) {
         var href = $(elt).attr('href');
-        $(elt).attr('href', href.replace('/'+token, ''));
+        $(elt).attr('href', href.replace('/' + token, ''));
     }
     $("#overlay").remove();
     $("#modalAttendable").remove();
