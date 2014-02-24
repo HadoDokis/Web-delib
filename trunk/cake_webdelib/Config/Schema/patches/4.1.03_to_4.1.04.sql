7b3e2b4de9e90e846a179ffca1171c2a7596c28d
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-02-24 16:27:13 +0000

    Correction : 	schéma sql de 4.1.04

--- trunk/cake_webdelib/Config/Schema/patches/4.1.03_to_4.1.04.sql
+++ trunk/cake_webdelib/Config/Schema/patches/4.1.03_to_4.1.04.sql
@@ -5,7 +5,7 @@ BEGIN;
 
 ALTER TABLE deliberations ADD COLUMN tdt_data_pdf bytea;
 
-ALTER TABLE deliberations ADD COLUMN tdt_data_bordereau_pdf bytea;
+ALTER TABLE deliberations RENAME COLUMN bordereau TO tdt_data_bordereau_pdf;
 
 --
 -- Fin du patch : fin de la transaction
