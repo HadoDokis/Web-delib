dbc62f5026a494d7da9fcecb438827a9ab0e4cf7
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-02-26 09:53:42 +0000

    amélioration affichage en arbre thémes

--- cake_webdelib/Controller/ThemesController.php
+++ cake_webdelib/Controller/ThemesController.php
@@ -2,105 +2,107 @@
 
 class ThemesController extends AppController {
 
-	var $name = 'Themes';
-	var $helpers = array('Html', 'Form', 'Session', 'Tree');
-	var $components = array('Droits');
+    public $name = 'Themes';
+    public $helpers = array('Html', 'Form', 'Session', 'Tree');
+    public $components = array('Droits');
 
-	// Gestion des droits
-	var $aucunDroit = array(
-			'getLibelle',
-			'isEditable',
-			'view'
-	);
-	var $commeDroit = array(
-			'edit'=>'Themes:index',
-			'add'=>'Themes:index',
-			'delete'=>'Themes:index'
-	);
+    // Gestion des droits
+    public $aucunDroit = array(
+        'getLibelle',
+        'isEditable',
+        'view'
+    );
+    public $commeDroit = array(
+        'edit' => 'Themes:index',
+        'add' => 'Themes:index',
+        'delete' => 'Themes:index'
+    );
 
-	function getLibelle ($id = null) {
-		$condition = "Theme.id = $id";
-		$objCourant = $this->Theme->find('first', array(
-				'conditions' => array('Theme.id' => $id),
-				'recursive'  => -1,
-				'fields'     => array('libelle')));
-		return $objCourant['Theme']['libelle'];
-	}
+    function getLibelle($id = null) {
+        $objCourant = $this->Theme->find('first', array(
+            'conditions' => array('Theme.id' => $id),
+            'recursive' => -1,
+            'fields' => array('libelle')));
+        return $objCourant['Theme']['libelle'];
+    }
 
-	function index() {
-		$themes =  $this->Theme->find('threaded',array('conditions'=>array('actif'=>1),
-				'order'=>'Theme.order ASC',
-				'recursive'=>-1));
-		$this->set('data', $themes);
-	}
+    function index() {
+        $themes = $this->Theme->find('threaded', array(
+            'conditions' => array('actif' => 1),
+            'order' => 'Theme.order ASC',
+            'recursive' => -1));
+        $this->set('data', $themes);
+    }
 
-	function view($id = null) {
-		if (!$id) {
-			$this->Session->setFlash('Invalide id pour le Th&egrave;me.', 'growl',array('type'=>'erreur'));
-			$this->redirect('/themes/index');
-		}
-		$this->set('theme', $this->Theme->read(null, $id));
-		$this->set('user_id',$this->Session->read('user.User.id'));
-		$this->set('Droits',$this->Droits);
-	}
+    function view($id = null) {
+        if (!$id) {
+            $this->Session->setFlash('Invalide id pour le Thême.', 'growl', array('type' => 'erreur'));
+            $this->redirect($this->referer());
+        }
+        $this->set('theme', $this->Theme->read(null, $id));
+        $this->set('user_id', $this->user_id);
+        $this->set('Droits', $this->Droits);
+    }
 
-	function add() {
-		$themes = $this->Theme->generateTreeList(array('Theme.actif' => '1'), null, null, '&nbsp;&nbsp;&nbsp;&nbsp;');
-		$this->set('themes', $themes);
-		if (!empty($this->data)) {
-			if (empty($this->data['Theme']['parent_id'])) 
-				$this->request->data['Theme']['parent_id']=0;
-			$this->request->data['Theme']['actif'] =1;
+    function add() {
+        $themes = $this->Theme->generateTreeList(array('Theme.actif' => '1'), null, null, '&nbsp;&nbsp;&nbsp;&nbsp;');
+        $this->set('themes', $themes);
+        if (!empty($this->data)) {
+            if (empty($this->data['Theme']['parent_id']))
+                $this->request->data['Theme']['parent_id'] = 0;
+            $this->request->data['Theme']['actif'] = 1;
 
-			if ($this->Theme->save($this->data)) {
-				$this->Session->setFlash('Le th&egrave;me a &eacute;t&eacute; sauvegard&eacute;', 'growl');
-				$this->redirect('/themes/index');
-			} else {
-				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.', 'growl',array('type'=>'erreur'));
-			}
-		}
-	}
+            if ($this->Theme->save($this->data)) {
+                $this->Session->setFlash('Le thème a été sauvegardé', 'growl');
+                $this->redirect($this->previous);
+            } else {
+                $this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.', 'growl', array('type' => 'erreur'));
+            }
+        }
+    }
 
-	function edit($id = null) {
-		if (empty($this->data)) {
-			if (!$id) {
-				$this->Session->setFlash('Invalide id pour le Th&egrave;me', 'growl',array('type'=>'erreur'));
-				$this->redirect('/Themes/index');
-			}
-			$this->data = $this->Theme->read(null, $id);
-			$themes = $this->Theme->generateTreeList(array('Theme.id <>'=> $id,'Theme.actif' => '1'), null, null, '&nbsp;&nbsp;&nbsp;&nbsp;');
-			$this->set('isEditable', $this->isEditable($id));
-			$this->set('themes', $themes);
-			$this->set('selectedTheme',$this->data['Theme']['parent_id']);
-		} else {
-			if (empty($this->data['Theme']['parent_id'])) $this->request->data['Theme']['parent_id']=0;
-			if ($this->Theme->save($this->data)) {
-				$this->Session->setFlash('Le th&egrave;me a &eacute;t&eacute; modifi&eacute;', 'growl');
-				$this->redirect('/themes/index');
-			} else {
-				$this->Session->setFlash('Veuillez corriger les erreurs ci-dessous.', 'growl',array('type'=>'erreur'));
-			}
-		}
-	}
+    function edit($id = null) {
+        if (empty($id)) {
+            $this->Session->setFlash('Invalide id pour le Thème', 'growl', array('type' => 'erreur'));
+            $this->redirect($this->referer());
+        }
+        if (!empty($this->request->data)) {
+            if (empty($this->request->data['Theme']['parent_id'])) $this->request->data['Theme']['parent_id'] = 0;
+            if ($this->Theme->save($this->request->data)) {
+                $this->Session->setFlash('Le thème a été modifié', 'growl');
+                return $this->redirect($this->previous);
+            } else {
+                $error_msg = 'Veuillez corriger les erreurs ci-dessous.';
+                if (empty($this->Theme->validationErrors)){
+                    $error_msg = 'Impossible de déplacer ce thème';
+                }
+                $this->Session->setFlash($error_msg, 'growl', array('type' => 'erreur'));
+            }
+        }
+        $this->data = $this->Theme->read(null, $id);
+        $themes = $this->Theme->generateTreeList(array('Theme.id <>' => $id, 'Theme.actif' => '1'), null, null, '&nbsp;&nbsp;&nbsp;&nbsp;');
+        $this->set('isEditable', $this->isEditable($id));
+        $this->set('themes', $themes);
+        $this->set('selectedTheme', $this->data['Theme']['parent_id']);
+    }
 
-	function delete($id = null) {
-		if (!$id) {
-			$this->Session->setFlash('Invalide id pour le Th&egrave;me', 'growl',array('type'=>'erreur'));
-			$this->redirect('/themes/index');
-		}
-		$theme = $this->Theme->read(null, $id);
-		$theme['Theme']['actif'] = 0;
-		if ( $this->Theme->save( $theme)) {
-			$this->Session->setFlash('Le Th&egrave;me a &eacute;t&eacute; d&eacute;sactiv&eacute;', 'growl');
-			$this->redirect('/themes/index');
-		}
-	}
+    function delete($id = null) {
+        if (!$id) {
+            $this->Session->setFlash('Invalide id pour le Thème', 'growl', array('type' => 'erreur'));
+            $this->redirect($this->referer());
+        }
+        $theme = $this->Theme->read(null, $id);
+        $theme['Theme']['actif'] = 0;
+        if ($this->Theme->save($theme)) {
+            $this->Session->setFlash('Le Thème a été désactivé', 'growl');
+            $this->redirect($this->referer());
+        }
+    }
 
-	function isEditable ($id) {
-		$liste = $this->Theme->find("first", array(
-				'conditions' => array('Theme.parent_id' => $id),
-				'recursive'  => -1));
-		return empty($liste);
-	}
+    function isEditable($id) {
+        $liste = $this->Theme->find("first", array(
+            'conditions' => array('Theme.parent_id' => $id),
+            'recursive' => -1));
+        return empty($liste);
+    }
 }
-?>
--- cake_webdelib/View/Themes/add.ctp
+++ cake_webdelib/View/Themes/add.ctp
@@ -1,16 +1,29 @@
-<h2>Ajout d'un th&egrave;me</h2>
-<?php echo $this->Form->create('Theme', array('url' => '/themes/add/','type'=>'post')); ?>
-<div class="optional">
- 	<?php echo $this->Form->input('Theme.libelle', array('label'=>'Libellé <acronym title="obligatoire">(*)</acronym>','size' => '50', 'maxlength' => '500'));?>
-</div>
-<div class="optional">
-        <?php echo $this->Form->input('Theme.order', array('label'=>'Crit&egrave;re de tri','size' => '10'));?>
-</div>
-<div>
-	<?php echo $this->Form->input('Theme.parent_id', array('label'=>'Appartient &agrave; ', 'options'=>$themes, 'empty'=>true, 'escape'=>false))?>
-</div>
-
+<h2>Ajout d'un thème</h2>
+<?php echo $this->Form->create('Theme', array('url' => '/themes/add/', 'type' => 'post')); ?>
+<?php echo $this->Form->input('Theme.libelle', array('label' => 'Libellé <abbr title="obligatoire">*</abbr>', 'maxlength' => '500')); ?>
+<?php echo $this->Form->input('Theme.order', array('label' => 'Critère de tri')); ?>
+<?php echo $this->Form->input('Theme.parent_id', array('label' => 'Appartient à', 'options' => $themes, 'empty' => true, 'escape' => false)) ?>
+<div class="spacer"></div>
 <div class="submit">
     <?php $this->Html2->boutonsAddCancel(); ?>
 </div>
 <?php echo $this->Form->end(); ?>
+
+<script>
+    $(document).ready(function () {
+        $("#ThemeParentId").select2({
+            width: 'resolve',
+            placeholder: 'Aucun',
+            allowClear: true,
+            formatSelection: function (object) {
+                // trim sur la sélection (affichage en arbre)
+                return $.trim(object.text);
+            }
+        });
+    });
+</script>
+<style>
+    #ThemeAddForm label {
+        padding-top: 5px;
+    }
+</style>
