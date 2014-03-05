6b385bf01ab72a4f9ad92ac2c05f5b85533e8bc8
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-03-05 09:47:47 +0000

    amélioration styles et vue listerFuturesSeances

---
+++ trunk/cake_webdelib/webroot/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Créé par Florian Ajir <florian.ajir@adullact.org> le 05/03/14.
+ *
+ * Instructions javascript à éxectuter au lancement de toutes les pages
+ */
+$(document).ready(function(){
+
+    $('a .fa-cog').click(function(){
+        $(this).addClass('fa-spin')
+    });
+
+});
