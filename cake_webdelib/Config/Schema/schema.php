15c9ecff304117b0b282f5840832c05268ac24ab
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-03-18 15:59:31 +0000

    delete colonne bordereau

--- cake_webdelib/Config/Schema/schema.php
+++ cake_webdelib/Config/Schema/schema.php
@@ -298,7 +298,6 @@ class AppSchema extends CakeSchema {
 		'vote_nb_retrait' => array('type' => 'integer', 'null' => true),
 		'vote_commentaire' => array('type' => 'string', 'null' => true, 'length' => 1000),
 		'delib_pdf' => array('type' => 'binary', 'null' => true),
-		'bordereau' => array('type' => 'binary', 'null' => true),
 		'signature' => array('type' => 'binary', 'null' => true),
 		'signee' => array('type' => 'boolean', 'null' => true),
 		'commission' => array('type' => 'binary', 'null' => true),
