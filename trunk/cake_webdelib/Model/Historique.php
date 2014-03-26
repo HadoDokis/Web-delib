10c933635e48bfcf464dc31a8af774be28a82980
florianajir <florianajir@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-03-26 13:40:54 +0000

    reformatage du code (lisibilité)

--- trunk/cake_webdelib/Model/Historique.php
+++ trunk/cake_webdelib/Model/Historique.php
@@ -1,37 +1,43 @@
 <?php
+
+/**
+ * Class Historique
+ */
 class Historique extends AppModel {
 
     var $name = 'Historique';
 
     var $belongsTo = array(
-                'Deliberation'=>array(
-                        'className'    => 'Deliberation',
-                        'conditions'   => '',
-                        'order'        => '',
-                        'dependent'    => false,
-                        'foreignKey'   => 'delib_id'),
-                'User'=>array(
-                        'className'    => 'User',
-                        'conditions'   => '',
-                        'order'        => '',
-                        'dependent'    => false,
-                        'foreignKey'   => 'user_id')
-			);
+        'Deliberation' => array(
+            'className' => 'Deliberation',
+            'conditions' => '',
+            'order' => '',
+            'dependent' => false,
+            'foreignKey' => 'delib_id'),
+        'User' => array(
+            'className' => 'User',
+            'conditions' => '',
+            'order' => '',
+            'dependent' => false,
+            'foreignKey' => 'user_id')
+    );
 
- 
+
     function enregistre($delib_id, $user_id, $commentaire) {
         $this->create();
-        $deliberation = $this->Deliberation->find('first', array('conditions' => array('Deliberation.id' => $delib_id),
-                                                                 'fields'     => array('id', 'circuit_id'),
-                                                                 'recursive'  => -1));
-	$user = $this->User->find('first', array('conditions' => array('User.id' => $user_id),
-                                                 'recursive'  => -1,
-                                                  'fields'    => array('User.nom', 'User.prenom')));
-        $histo['Historique']['user_id']     = $user_id;
-        $histo['Historique']['circuit_id']  = $deliberation['Deliberation']['circuit_id'];
-        $histo['Historique']['delib_id']    = $delib_id;
-        $histo['Historique']['commentaire'] = "[".$user['User']['prenom'].' '.$user['User']['nom']."] $commentaire";
-        return $this->save($histo); 
+        $deliberation = $this->Deliberation->find('first', array(
+            'conditions' => array('Deliberation.id' => $delib_id),
+            'fields' => array('id', 'circuit_id'),
+            'recursive' => -1));
+        $user = $this->User->find('first', array(
+            'conditions' => array('User.id' => $user_id),
+            'recursive' => -1,
+            'fields' => array('User.nom', 'User.prenom')));
+        $histo['Historique']['user_id'] = $user_id;
+        $histo['Historique']['circuit_id'] = $deliberation['Deliberation']['circuit_id'];
+        $histo['Historique']['delib_id'] = $delib_id;
+        $histo['Historique']['commentaire'] = "[" . $user['User']['prenom'] . ' ' . $user['User']['nom'] . "] $commentaire";
+        return $this->save($histo);
     }
 
     /**
@@ -51,12 +57,11 @@ class Historique extends AppModel {
         if (empty($historiques)) return;
 
         $oStyleIteration = new GDO_IterationType("Historique");
-        foreach($historiques as $historique) {
+        foreach ($historiques as $historique) {
             $oDevPart = new GDO_PartType();
-            $oDevPart->addElement(new GDO_FieldType("log", $historique['Historique']['created'].' : '.$historique['Historique']['commentaire'], "text"));
+            $oDevPart->addElement(new GDO_FieldType("log", $historique['Historique']['created'] . ' : ' . $historique['Historique']['commentaire'], "text"));
             $oStyleIteration->addPart($oDevPart);
         }
         $oMainPart->addElement($oStyleIteration);
     }
 }
-?>
