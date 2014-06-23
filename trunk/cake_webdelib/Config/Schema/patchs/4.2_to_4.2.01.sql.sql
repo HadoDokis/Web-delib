be3ac9711651e8a0883f68360ca6715aa3dae709
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-06-23 08:11:12 +0000

    Ajout :  Récupération des messages Tdt

--- trunk/cake_webdelib/Config/Schema/patchs/4.2_to_4.2.01.sql.sql
+++ trunk/cake_webdelib/Config/Schema/patchs/4.2_to_4.2.01.sql.sql
@@ -9,8 +9,11 @@ ALTER TABLE tdt_messages RENAME type_reponse TO tdt_etat;
 ALTER TABLE tdt_messages RENAME type_message TO tdt_type;
 ALTER TABLE tdt_messages RENAME "data" TO tdt_data;
 
-
+-- Ajout de la tdt_AR
 ALTER TABLE deliberations ADD COLUMN tdt_AR bytea;
+
+-- Mise à jour du nom et de la description de la tache de conversion des annexes
+UPDATE crons SET nom='CONVERSION : conversion des annexes', description='conversion des annexes dans différents formats' WHERE id=7;
 --
 -- Fin du patch : fin de la transaction
 --
