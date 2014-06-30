dc1968cf046a13ec6a9418446480cacd390ab191
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2014-06-30 08:18:46 +0000

    Correction :  Récupération des messages Tdt

--- cake_webdelib/Config/Schema/patchs/4.2_to_4.2.01.sql
+++ cake_webdelib/Config/Schema/patchs/4.2_to_4.2.01.sql
@@ -8,7 +8,7 @@ ALTER TABLE tdt_messages RENAME message_id TO tdt_id;
 ALTER TABLE tdt_messages RENAME type_reponse TO tdt_etat;
 ALTER TABLE tdt_messages RENAME type_message TO tdt_type;
 ALTER TABLE tdt_messages RENAME "data" TO tdt_data;
-
+ALTER TABLE tdt_messages ADD COLUMN parent_id integer;
 -- Ajout de la tdt_AR
 ALTER TABLE deliberations ADD COLUMN tdt_AR bytea;
 
