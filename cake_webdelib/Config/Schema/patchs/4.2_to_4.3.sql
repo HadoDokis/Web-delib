bf82afa5a2d61c0f8b7f70370ff5597a82195042
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2015-02-24 15:05:32 +0000

    Modification : Mise en palce de la nouvelle gestion des droits

--- cake_webdelib/Config/Schema/patchs/4.2_to_4.3.sql
+++ cake_webdelib/Config/Schema/patchs/4.2_to_4.3.sql
@@ -5,5 +5,13 @@ BEGIN;
 
 UPDATE acos SET alias='Seances:Calendrier' WHERE alias ='Seances:afficherCalendrier';
 ALTER TABLE typeseances ADD COLUMN color VARCHAR (7);
---
+
+ALTER TABLE users RENAME COLUMN login to username;
+ALTER TABLE users ADD COLUMN active boolean DEFAULT true;
+UPDATE users SET active=true;
+
+ALTER TABLE profils RENAME COLUMN libelle to "name";
+
+ALTER TABLE typeactes RENAME COLUMN libelle to "name";
+
 COMMIT;
