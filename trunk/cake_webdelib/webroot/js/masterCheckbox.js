1af7d45ab8e0ebc832e963d875f00a0f762079de
splaza <splaza@062030ea-4d40-4e01-b90d-eb5f47022e4e>
2015-02-24 15:04:22 +0000

    Modification : Mise en palce de la nouvelle gestion des droits

--- trunk/cake_webdelib/webroot/js/masterCheckbox.js
+++ trunk/cake_webdelib/webroot/js/masterCheckbox.js
@@ -19,18 +19,6 @@ $(document).ready(function () {
     $("#masterCheckbox").prop('checked', $("input[type=checkbox]").not(':disabled').not(':checked').not("#masterCheckbox"));
     $("#masterCheckbox").prop('disabled', !$("input[type=checkbox]").not(':disabled').not("#masterCheckbox").length);
 
-    //Master-checkbox spécifique à la table des droits
-    // La master checkbox coche/décoche tout d'un coup
-    $("#masterCheckboxDroits").change(function () {
-        $("#tableDroits input[type=checkbox]").not(':disabled').prop('checked', $(this).prop('checked'));
-    });
-    //Checkbox -> masterCheckbox
-    $('#tableDroits input[type="checkbox"]').not("#masterCheckboxDroits").change(function () {
-        $('#masterCheckboxDroits').prop('checked', $('#tableDroits input[type="checkbox"]').not('#masterCheckboxDroits').not(':disabled').not(':checked').length === 0);
-    });
-    // Si toutes les checkboxes (sauf masterCheckbox) sont cochées, cocher masterCheckbox
-    $("#masterCheckboxDroits").prop('checked', $("#tableDroits input[type=checkbox]").not(':disabled').not(':checked').not("#masterCheckboxDroits").length === 0);
-    $("#masterCheckboxDroits").prop('disabled', !$("#tableDroits input[type=checkbox]").not(':disabled').not("#masterCheckboxDroits").length);
 });
 
 function selectAll() {
